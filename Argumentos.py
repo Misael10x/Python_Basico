2a9014b5e8b3b99e9d8ab89c590c08846e60d45d
Misael10x <misabar12389@gmail.com>
2024-11-01 22:48:42 -0600

    update

---
+++ Argumentos.py
@@ -0,0 +1,8 @@
+# Argumentos
+# son argumentos que van predecidos por un identificador cuando se pasan a una funcion
+
+def saludo(nombre, apellido, lenguaje):
+    print('Hola ' + nombre + ' ' + apellido + ' ,estas aprendiendo: ' + lenguaje)
+    
+saludo(lenguaje = 'Python', nombre='Misael', apellido='Barbosa')
+    
