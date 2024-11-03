32805af4509490abc9cb78de9a068d60b62dbfbc
Misael10x <misabar12389@gmail.com>
2024-11-02 21:38:17 -0600

    update

---
+++ AsignacionMutiple.py
@@ -0,0 +1,25 @@
+## Ejemplo 1
+
+# nombre = "Misael"
+# edad = 22
+# atractivo = True
+
+#nombre, edad, atractivo = "Misael", 22, True # forma abreviada
+ 
+# print(nombre)
+# print(edad)
+# print(atractivo)
+
+## Ejemplo 2
+
+# nombre1 = 10
+# nombre2 = 10
+# nombre3 = 10
+# nombre4 = 10
+
+nombre1 = nombre2 = nombre3 = nombre4 = 10  # Forma abreviada
+
+print(nombre1)
+print(nombre2)
+print(nombre3)
+print(nombre4)
