287b9247b99ed1390a8584022252c890b1d232e0
Misael10x <misabar12389@gmail.com>
2024-11-02 22:11:12 -0600

    update

---
+++ automovil.py
@@ -0,0 +1,15 @@
+class Auto:
+    
+    def __init__(self, marca, modelo, ano, color):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.color = color
+    
+    def encendido(self):
+        print('El auto esta encendido')
+        
+    def apagado(self):
+        print('El auto esta apagado')
+        
+        
