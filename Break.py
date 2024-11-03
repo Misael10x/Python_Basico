98dc8586e22bb79817465905674a0091128fef8d
Misael10x <misabar12389@gmail.com>
2024-11-02 22:12:45 -0600

    update

---
+++ Break.py
@@ -0,0 +1,28 @@
+# Break continue pass
+
+#   Break determina completamente el buncle en otras parabas sirve para cerrar el buncle por completo
+
+#nombre = ""
+
+#while True:
+    #nombre = input("Ingresa tu nombre: ")
+    #if nombre != "":
+        #break
+        
+#   Continue salta la siguiente interacion de bucle
+
+#telefono = "123-456-789"
+
+#for i in telefono:
+    #if i == "-":
+        #continue
+    #print(i, end="")
+    
+#   Pass no hace nada solo actua como marcador de posicion
+
+for i in range(1,21):
+    if i == 13:
+        pass
+    else:
+        print(i)
+
