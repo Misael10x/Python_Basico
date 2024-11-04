c074d2e643d4b3ca69fd617626a4fd77866c5ed3
Misael10x <misabar12389@gmail.com>
2024-11-03 19:59:10 -0600

    update

---
+++ CTD.py
@@ -0,0 +1,21 @@
+## Convertir tipos de datos 
+
+# x = 5 # entero
+# y = 2.0 # float
+# z = "3" # string 
+
+# y = int(y)
+# x = int(x)
+
+
+# print(x*y)
+# print(y)
+# print(z)
+
+
+## Ejemplo 2
+
+edad = 22
+
+print("Edad " + str(edad))
+print(type(edad))
