3bf245c34cd63e00d40671b7e345c3b049216fef
Misael10x <misabar12389@gmail.com>
2024-11-04 18:15:32 -0600

    update

---
+++ For.py
@@ -0,0 +1,22 @@
+## Bucles For
+
+#Ejemplo
+#for x in range(56, 101, 2):
+     #print(x)
+
+#Ejercicio 1
+#nombre = input("Ingresa tu nombre: ")
+
+#for x in nombre:
+    #print(x)
+    
+#Ejercicio 2 cuenta regresiva del 10 al 0
+
+import time
+
+for s in range(10, 0, -1): #el -1 es el salto de linea
+    print(s)
+    time.sleep(1) #un segundo
+    
+print("Feliz año nuevo :)")
+print("Felicidades Misael:)")
