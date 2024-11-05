04338e4cd787fb0b3328332ca2a748c15cab3eef
Misael10x <misabar12389@gmail.com>
2024-11-04 18:12:54 -0600

    update

---
+++ FA.py
@@ -0,0 +1,10 @@
+# Funciones Anidadas
+
+#num = input("Ingrese un numero: ")
+#num = float(num) #convertir el numero a flotante
+#num = abs(num) #convertir el valor en absoluto
+#num = round(num) #vamos a rendondear ese valor
+#print(num)
+
+print(round(abs(float(input("Ingrese un NUmero: ")))))
+
