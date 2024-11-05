78564b153f82cf344385f7039f35c784fea3afd9
Misael10x <misabar12389@gmail.com>
2024-11-04 18:13:24 -0600

    update

---
+++ FM.py
@@ -0,0 +1,23 @@
+## Funciones matematicas 
+import math # Importamos la libreria math
+
+PI = 3.14
+
+x = 2
+y = 1
+z = 3
+
+# print(round(PI))
+
+# print(math.ceil) #ceil rendondea hacia arriba
+
+# print(math.floor(PI)) #floor sirve para redondear hacia abajo
+
+# print(abs(PI)) #abs sirve para darnos el valor absoluto de un numero
+
+# print(pow(PI, 2)) #pow Sirve para elevar un numero base a una potencia
+
+# print(math.sqrt(420)) #sqrt Sirve para encontrar la raiz cuadrada de un numero
+
+print(max(x, y, z)) # Sirve para sacar el maximo y el minimo
+print(min(x, y, z)) # Sirve para sacar el maximo y el minimo
