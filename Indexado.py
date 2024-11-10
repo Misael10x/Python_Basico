9ad8cda6f4340185b974746147433e3f25ea78c8
Misael10x <misabar12389@gmail.com>
2024-11-09 19:32:13 -0600

    update

---
+++ Indexado.py
@@ -0,0 +1,16 @@
+# Indexar y segmentar cadenas
+
+nombre = 'misael barbosa!'
+
+#if nombre[0].islower():
+    #nombre = nombre.capitalize()
+    
+primer_nombre = nombre[:6].upper() # Mayusculas con upper
+apellido = nombre[7:].lower() # minisculas con lower
+ultimo_caracter = nombre [-1] # Ultimo caracter de la cadena
+
+print(primer_nombre) 
+print(apellido)
+    
+#print(nombre)
+
