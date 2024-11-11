468998e3e9326b4741a24766440900943caa3be7
Misael10x <misabar12389@gmail.com>
2024-11-10 19:18:30 -0600

    update

---
+++ Lista2D.py
@@ -0,0 +1,9 @@
+# LISTAS 2D
+
+bebidas = ["cafe","soda","cafe","cerveza"]
+cena = ["pizza","hamburguesa","hot dog"]
+postres = ["pastel","helado"]
+
+comida = [bebidas, cena, postres]
+
+print(comida[1][2]) 
