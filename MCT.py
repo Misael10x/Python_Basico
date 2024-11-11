b7c0a4d0b158f5d48a1e2a17e6123bd58e696e56
Misael10x <misabar12389@gmail.com>
2024-11-10 19:20:01 -0600

    update

---
+++ MCT.py
@@ -0,0 +1,15 @@
+# "Metodo de cadena de textos"
+
+nombre = "Misael"
+
+# print(len(nombre)) # saber la longitud de la cadena
+# print(nombre.find("e")) # Metodo de busqueda
+# print(nombre.capitalize()) # Metodo capitalize pero antes todas las letras del nombre deben ser minusculas
+# capitalize capitaliza la primera letra osea la coloca en mayuscula
+# print(nombre.upper()) # metodo upper sirve para convertir toda la cadena a mayusculas
+# print(nombre.lower()) # Metodo lower para convertir en minusculas todo el texto
+# print(nombre.isdigit()) # Metodo isdigit sirve para saber si es un digito o no.
+# print(nombre.isalpha()) # Metodo isalphasirve para ver si hay espacios o caracteres especiales
+# print (nombre.count("a")) # Metodo count sirve para conocer cuanta letras hay en nuestra cadena
+# print(nombre.replace("c", "l")) # Metodo replace sirve para reemplazar caracteres dentro de una cadena
+print(nombre*5) # Tip/Caracteristica Mostrar una cadena varias veces 
