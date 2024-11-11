30769fa13b321d9b02bd7d01d41dc80875765064
Misael10x <misabar12389@gmail.com>
2024-11-10 19:18:03 -0600

    update

---
+++ Listas.py
@@ -0,0 +1,19 @@
+# Listas sirve para almacenar multiples elementos dentro de una sola varible
+comida = ['pizza', 'hamburguesa', 'hot dog', 'spagueti', 'puding', 2, 0.5, False] # almacena distintos tipos de datos
+
+comida[0] = 'helado'
+
+#comida.append('sushi')     agregar un elemento a la lista al final
+#comida.remove('hamburguesa')       remover un elemento especifico de la lista
+#comida.pop()       eliminar el ultimo elemento de la lista
+#comida.insert(0, "pastel") inserta un valor en una cierta posicion especifica
+#comida.sort()  ordena la lista alfabeticamente
+#comida.clear()# elimina todos los elemento de la lista
+
+
+
+for x in comida:
+    print(x)
+    
+    
+    #print(comida[0])
