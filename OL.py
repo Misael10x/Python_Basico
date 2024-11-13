25c9e389752cd9bb28dbe228af7e56185b672351
Misael10x <misabar12389@gmail.com>
2024-11-12 19:58:22 -0600

    update

---
+++ OL.py
@@ -0,0 +1,18 @@
+# Operadores logicos AND, OR y NOT
+
+# Crear un programa que solicite al usuario la temperatura de afuera
+
+temperatura = int(input("¿Cual es la temperatura afuera? "))
+
+if not(temperatura >= 0 and temperatura <=30):
+    print("La temperatura esta bien hoy")
+    
+    
+elif not(temperatura < 0 or temperatura > 30):
+    print("La temperatura no esta bien hoy")
+    
+    # NOT cambia de verdadero a falso o de falso a verdadero
+    
+
+    
+
