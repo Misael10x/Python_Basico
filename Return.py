f542a9a26cc8c1309a6250051f7cae7ab062fc8a
Misael10x <misabar12389@gmail.com>
2024-11-13 20:28:34 -0600

    update

---
+++ Return.py
@@ -0,0 +1,8 @@
+# Return 
+
+# se utiliza para enviar valores de regreso a donde se han llamado osea el valor de rtorno de la funcion
+
+def multiplicar(numero_uno, numero_dos):
+    return numero_uno * numero_dos
+x = (multiplicar(6, 8))
+print(x)
