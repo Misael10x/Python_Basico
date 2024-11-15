f13484e5cf4edd96ba4df293d7cc12463d1c47cd
Misael10x <misabar12389@gmail.com>
2024-11-14 19:05:47 -0600

    update

---
+++ Sets.py
@@ -0,0 +1,20 @@
+# Sets
+# 1 no puede hacer elemetos duplicados 
+# 2 no mantienen el orden de cuando son declarados 
+# 3 sus elementos deben ser inmutables
+
+utensilios = {"tenedor", "cuchara", "cuchillo"}
+platos = {"plato", "bol", "taza", "cuchara"}
+
+#utensilios.add("cucharita")
+#utensilios.remove("cuchara")
+#utensilios.pop()
+#utensilios.clear()
+#utensilios.update(platos)
+print(utensilios.intersection(platos))
+#print(utensilios.difference(platos))
+
+#for x in utensilios:
+    #print(x)
+    
+    
