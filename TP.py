64f2e0d732fb2e93bab8e5962515edeea301cbad
Misael10x <misabar12389@gmail.com>
2024-11-14 19:07:45 -0600

    update

---
+++ TP.py
@@ -0,0 +1,11 @@
+## Ingresar textos 
+
+nombre = input("¿Cual es tu nombre? ")
+edad = int(input("¿Cuantos años tienes? "))
+altura = float(input("¿Cual es tu altura? "))
+
+#edad = edad + 1
+
+print("Hola " + nombre)
+print("Tienes: " + str(edad) + " años")
+print("Mides: " + str(altura) + "cm")
