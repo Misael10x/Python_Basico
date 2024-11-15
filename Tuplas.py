be2ab867465b779ef4c0c6418337a10611ff7381
Misael10x <misabar12389@gmail.com>
2024-11-14 19:08:18 -0600

    update

---
+++ Tuplas.py
@@ -0,0 +1,12 @@
+# Tuplas
+
+estudiantes = ('misael', 22, 'M')
+
+#print(estudiantes.count('misael'))
+#print(estudiantes.index('M'))
+
+#for x in estudiantes:
+    #print(x)
+    
+if 'misael' in estudiantes:
+    print("misael esta aqui")
