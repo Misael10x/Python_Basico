1142e7d7bc6cf5b7e6d302d5a67242b096b0499a
Misael10x <misabar12389@gmail.com>
2024-11-15 18:43:47 -0600

    update

---
+++ Variable.py
@@ -0,0 +1,12 @@
+class Auto:
+    rueda = 4 # variable de clase
+    
+    def __init__(self, marca, modelo, ano, color):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.color = color
+    
+    
+        
+        
