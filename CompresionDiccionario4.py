18887cd64d7b83d72bcffc5ed0fe725a568dd473
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2024-12-15 20:18:53 -0600

    Update CompresionDiccionario4.py

--- CompresionDiccionario4.py
+++ CompresionDiccionario4.py
@@ -1,1 +1,12 @@
+def check_temp(value):
+    if value >=70:
+        return "CALOR"
+    elif 60 >= value >=40:
+        return "Normal"
+    else:
+     return "FRIO"
+     
 
+ciudades = {'New York': 32, 'Boston': 75, 'Los Angeles': 100, 'Chicago': 50}
+clima = {key: check_temp(value) for (key, value) in ciudades.items()}
+print(clima)
