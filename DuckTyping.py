b43e5728eb30b105c3c953f0ea1da1562704165e
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2024-12-16 20:16:04 -0600

    Update DuckTyping.py

--- DuckTyping.py
+++ DuckTyping.py
@@ -1,1 +1,27 @@
+class Pato:
+    
+    def caminar(self):
+        print('Este pato esta caminado')
+        
+    def hablando(self):
+        print('este pato dice cuac')
+        
+        
+class Gallina:
+    def caminar(self):
+        print('Esta gallina esta caminado')
+        
+    def hablando(self):
+        print('Esta gallina esta cacareando')
+        
+class Persona:
+    def atrapar(self,pato):
+        pato.caminar()
+        pato.hablando()
+        print('Lo atrapastes')
+        
+pato = Pato()
+gallina = Gallina()
+persona = Persona()
 
+persona.atrapar(gallina)
