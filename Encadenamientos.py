7aa5220309fb8caa4dd2487700cc6ceb93a14622
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2024-12-17 21:52:54 -0600

    Update Encadenamientos.py

--- Encadenamientos.py
+++ Encadenamientos.py
@@ -1,1 +1,30 @@
-
+class Coche:
+    def encender(self):
+        print('Haz arracado el motor')
+        return self
+        
+    def conducir(self):
+        print('Estas conduciendo')
+        return self
+        
+    def frenar(self):
+        print('Estas frenando')
+        return self
+        
+    def apagar(self):
+        print('Apagaste el motor')
+        return self
+        
+coche = Coche() 
+#       METODO 1
+#coche.encender().conducir()
+ 
+ #      METODO 2
+#coche.frenar().apagar()
+ 
+ #      METODO 3
+coche.encender()\
+.conducir()\
+.frenar()\
+.apagar()
+ #podemos llamar varios metodos secuencialmente
