a858f52ff56e652971a34a52ffac261c3b18fd9d
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2024-12-18 19:16:04 -0600

    Create FuncionesLambda4.py

---
+++ FuncionesLambda4.py
@@ -0,0 +1,9 @@
+doble = lambda x: x * 2
+
+multiplicar = lambda x, y: x * y
+
+suma = lambda x, y, z: x * y * z
+
+nombre_completo = lambda nombre, apellido: nombre + " " + apellido
+
+print(nombre_completo("Misael","Barbosa"))
