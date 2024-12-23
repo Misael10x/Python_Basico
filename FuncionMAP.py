ccd4f5189ec3afc18afaf8a74b001df62ea14675
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2024-12-22 19:41:12 -0600

    Update FuncionMAP.py

--- FuncionMAP.py
+++ FuncionMAP.py
@@ -2,3 +2,6 @@ tienda = [("Camisa", 20.00),
           ("Pantalones", 25.00),
           ("Chaqueta", 50.00),
           ("Medias", 10.00)]
+
+fun_euro = lambda datos: (datos[0], datos[1]*0.96)
+fun_dolar = lambda datos: (datos[0], datos[1]/0.96)
