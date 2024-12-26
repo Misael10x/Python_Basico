28266d774ceaf54669c39316e9c3f268b0244ac0
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2024-12-25 21:18:41 -0600

    Update Funcion_Super.py

--- Funcion_Super.py
+++ Funcion_Super.py
@@ -1,1 +1,5 @@
-
+#se utiliza para hacer a un metodo padre y devuelve un objeto temporal
+class Rectangulo:
+    def __init__(self, alto, ancho):
+        self.alto = alto
+        self.ancho = ancho
