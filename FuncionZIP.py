65f335797f318bb7a08a9de4438802d21d1a8524
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2024-12-26 19:03:35 -0600

    Update FuncionZIP.py

--- FuncionZIP.py
+++ FuncionZIP.py
@@ -1,3 +1,6 @@
 nombre_usuario = ["Misael", "Barbosa", "Hermano"]
 contrasena = ("123", "abc", "contra")
 fecha_inicio_sesion = ['01/04/2023', '21/12/2023', '10/06/2023']
+
+
+usuarios = list(zip(nombre_usuario, contrasena, fecha_inicio_sesion))
