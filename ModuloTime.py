bb95e269dc9eb8c04b5c3cae59f6d4fdd51694f9
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2024-12-30 19:56:07 -0600

    Update ModuloTime.py

--- ModuloTime.py
+++ ModuloTime.py
@@ -6,3 +6,16 @@ import time
 #print(time.time())
 
 #print(time.ctime(time.time()))
+
+#tiempo_actual = time.localtime()
+#print(tiempo_actual)
+
+#tiempo_actual = time.localtime()
+
+#tiempo_actual = time.gmtime()
+#tiempo = time.strftime("%B %d %y %H:%M:%S", tiempo_actual)
+#print(tiempo)
+
+#cadena_tiempo = '18 November, 2024'
+#tiempo = time.strptime(cadena_tiempo, '%d %B, %Y')
+#print(tiempo)
