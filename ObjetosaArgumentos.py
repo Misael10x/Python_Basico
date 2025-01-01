9037bbd47ecda6fb09ac3ccce969a8f4d9855008
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2024-12-31 19:44:45 -0600

    Update ObjetosaArgumentos.py

--- ObjetosaArgumentos.py
+++ ObjetosaArgumentos.py
@@ -11,3 +11,12 @@ class Motocicleta:
     def cambiar_color(vehiculo, color):
         vehiculo.color = color
     
+coche1 = Coche()
+coche2 = Coche()
+coche3 = Coche()
+motocicleta = Motocicleta()
+
+cambiar_color(coche1, 'rojo')
+cambiar_color(coche2, 'amarillo')
+cambiar_color(coche3, 'verde')
+cambiar_color(motocicleta, 'azul')
