2b57ac79ad261c638160859f065f7095ab11228a
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-02 19:37:55 -0600

    Update OrdenarIterales.py

--- OrdenarIterales.py
+++ OrdenarIterales.py
@@ -4,3 +4,6 @@ estudiantes = ["Misael", "Abigail", "Bryan", "Kevin", "Lupita"]
 
 estudiantes.sort(reverse=True)
 #estudiantes2 = sorted(estudiantes, reverse=True)
+
+for i in estudiantes:
+    print(i)
