8c13970a3e167d14603c254dfb17f4f4e4e01504
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-05 20:09:12 -0600

    Update ModuloThreading.py

--- ModuloThreading.py
+++ ModuloThreading.py
@@ -16,3 +16,30 @@ def estudiar():
     print('Iniciando...estuduar')
     time.sleep(5)
     print('Finalizando...')
+
+inicio = time.perf_counter()
+
+x = threading.Thread(target=desayunar, args=())
+x.start()
+
+y = threading.Thread(target=tomar_cafe, args=())
+y.start()
+
+
+z = threading.Thread(target=estudiar, args=())
+z.start()
+
+x.join() # sincronizacion de hilos
+y.join()
+z.join()
+
+#desayunar()
+#tomar_cafe()
+#estudiar()
+
+print(threading.active_count())
+print(threading.enumerate())
+
+fin = time.perf_counter()
+tiempo = fin - inicio
+print(tiempo)
