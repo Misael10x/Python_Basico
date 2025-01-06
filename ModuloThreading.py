a4b20f8bdffcc9431816388534327fe67fe95df4
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-05 20:07:39 -0600

    Update ModuloThreading.py

--- ModuloThreading.py
+++ ModuloThreading.py
@@ -1,1 +1,18 @@
+import threading
+import time
 
+
+def desayunar():
+    print('Iniciando...desayuno')
+    time.sleep(3)
+    print('Finalizando...')
+
+def tomar_cafe():
+    print('Iniciando...tomar cafe')
+    time.sleep(4)
+    print('Finalizando...')
+    
+def estudiar():  
+    print('Iniciando...estuduar')
+    time.sleep(5)
+    print('Finalizando...')
