f4fa7b1c5ce8b93e3d1762bf3056441a1006a2a3
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-06 20:29:46 -0600

    Update HilosDemonios.py

--- HilosDemonios.py
+++ HilosDemonios.py
@@ -1,2 +1,11 @@
 import threading
 import time
+
+def tiempo():
+    print()
+    print()
+    contador = 0
+    while True:
+        time.sleep(1)
+        contador += 1
+        print(contador, "Segundos")
