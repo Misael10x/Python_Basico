a7024051a3698b7fd616a97bb3fa68b0325c33d8
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-06 20:31:04 -0600

    Update HilosDemonios.py

--- HilosDemonios.py
+++ HilosDemonios.py
@@ -14,3 +14,11 @@ def tiempo():
 x = threading.Thread(target=tiempo, daemon=True)
 #x.setDaemon(False)
 x.start()
+
+
+print(x.isDaemon)
+
+entrada = input('Desea salir?')
+    
+    
+# sirve para tareas de segundo plano, recoleccion de basura, esperar entradas o procesos de larga duracion
