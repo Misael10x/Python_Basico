5dba0f85f7ead335774c355ab37ad464574b655c
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-09 22:03:31 -0600

    Update Multiprocesamientos.py

--- Multiprocesamientos.py
+++ Multiprocesamientos.py
@@ -10,3 +10,15 @@ def contador(num):
 # cada maquina tiene un limite de nucleos si se pasa el limite deellos se hace mas lento
 #ya que hasta que los nucleos se desacupen o se desacupen uno de ellos seguira el siguiente
 
+
+def main():
+    
+    iniciar = time.perf_counter()
+    a = Process(target=contador, args=(12500000,))
+    b = Process(target=contador, args=(125000000,))
+    c = Process(target=contador, args=(125000000,))
+    d = Process(target=contador, args=(125000000,))
+    e = Process(target=contador, args=(125000000,))
+    f = Process(target=contador, args=(125000000,))
+    g = Process(target=contador, args=(125000000,))
+    h = Process(target=contador, args=(125000000,))
