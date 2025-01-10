ec06e36554a229faeb36d66eb3f05f2f34b58544
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-09 22:03:11 -0600

    Update Multiprocesamientos.py

--- Multiprocesamientos.py
+++ Multiprocesamientos.py
@@ -1,1 +1,12 @@
+from multiprocessing import Process, cpu_count
+import time
+
+
+def contador(num):
+    cont = 0
+    while cont < num:
+        cont += 1
+
+# cada maquina tiene un limite de nucleos si se pasa el limite deellos se hace mas lento
+#ya que hasta que los nucleos se desacupen o se desacupen uno de ellos seguira el siguiente
 
