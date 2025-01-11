861a797092a34a728ae9c00b49d7ff1b32bfa2a2
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-10 22:52:30 -0600

    Update InterfazGrafica.py

--- InterfazGrafica.py
+++ InterfazGrafica.py
@@ -3,3 +3,8 @@ from tkinter import *
 windows = Tk()
 
 windows.geometry("600x600")
+
+windows.title('Bienvenido al mundo de la Programacion conm Python')
+
+icono = PhotoImage(file='logo2.png')
+windows.iconphoto(True, icono)
