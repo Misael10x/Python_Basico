52a7b2a7f81ef55e96a3c31522380243d557edda
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-11 22:49:28 -0600

    Update EtiquetasThinker.py

--- EtiquetasThinker.py
+++ EtiquetasThinker.py
@@ -1,3 +1,12 @@
 from tkinter import *
 
 Window = Tk()
+
+Label = Label(Window, text='Hola, Bienvenido Misael :)',
+              font=('Arial',40,'bold'),
+              fg='green',
+              bg='black',
+              relief='groove',
+              bd=20,
+              padx=50,
+              pady=50)
