b9fafbf21a9af3edc1e7b7cae05ed0acf9e385e8
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-14 22:53:52 -0600

    Update CrearBoton.py

--- CrearBoton.py
+++ CrearBoton.py
@@ -7,3 +7,17 @@ def click():
 windows = Tk()
 
 imagen = PhotoImage(file='pulso.png')
+
+boton = Button(windows, text='Haz click', command=click,
+               font=('Arial',20,'bold'),
+               fg='red',
+               bg='black',
+               activeforeground='red',
+               activebackground='#fff',
+               state='active',
+               relief='raised',
+               bd=10,
+               padx=15,
+               pady=15,
+               image=imagen,
+               compound='top')
