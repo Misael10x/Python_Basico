63de57c929d0951bb03bc1e4a9d4ed9ffeda935c
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-15 21:08:43 -0600

    Update Multiplos.py

--- Multiplos.py
+++ Multiplos.py
@@ -2,3 +2,4 @@ def encontrar_multiplos(numero, limite):
     if numero <= 0 or limite <= 0:
         raise ValueError("El número y el límite deben ser mayores que cero.")
     
+    return [i for i in range(numero, limite + 1, numero)]
