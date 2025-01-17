8c4cb156ff9eb66fe139b3471cae55bf56dd2be0
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-16 20:42:42 -0600

    Update WidgetEntrada.py

--- WidgetEntrada.py
+++ WidgetEntrada.py
@@ -1,1 +1,15 @@
 from tkinter import *
+
+def enviar():
+    nombre_usuario = entrada.get()
+    print(f"Hola {nombre_usuario}")
+    entrada.config(state=DISABLED)
+
+def reset():
+    entrada.config(state=NORMAL)
+    entrada.delete(0, END)
+
+def delete():
+    actual_texto = entrada.get()
+    if actual_texto:
+        entrada.delete(len(actual_texto) - 1, END)
