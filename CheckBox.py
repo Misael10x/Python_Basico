8fb6cbdb779c6fa586833e6b8af892972323a71c
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-17 20:13:54 -0600

    Update CheckBox.py

--- CheckBox.py
+++ CheckBox.py
@@ -15,3 +15,21 @@ x = IntVar()
 original_image = Image.open('logo.png')
 resized_image = original_image.resize((50, 50)) 
 python_photo = ImageTk.PhotoImage(resized_image)
+
+
+check_button = Checkbutton(windows,
+                           text='Acepto',
+                           variable=x,
+                           onvalue=1,
+                           offvalue=0,
+                           command=mostrar,
+                           font=('Arial', 20),
+                           fg='red',
+                           bg='black',
+                           activeforeground='red',
+                           activebackground='black',
+                           padx=20,
+                           pady=25,
+                           image=python_photo,
+                           compound='left')
+                
