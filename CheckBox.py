d55ea7b0e687c9f4c45eabcb9a6e1488859e7ec9
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-17 20:13:19 -0600

    Update CheckBox.py

--- CheckBox.py
+++ CheckBox.py
@@ -6,3 +6,12 @@ def mostrar ():
         print('Estoy de acuerdo')
     else:
         print('No estas deacuerdo')
+
+windows = Tk()
+
+
+x = IntVar()
+
+original_image = Image.open('logo.png')
+resized_image = original_image.resize((50, 50)) 
+python_photo = ImageTk.PhotoImage(resized_image)
