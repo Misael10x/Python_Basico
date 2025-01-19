043db503b65c23435841211c1c9b7400f36caa78
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-18 20:57:20 -0600

    Update RadioBoton.py

--- RadioBoton.py
+++ RadioBoton.py
@@ -2,3 +2,13 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 comida = ['Pizza', 'Hamburguesa', 'Hotdog']
+
+def orden():
+    if x.get() == 0:
+        print('Haz ordenado una Pizza')
+    elif x.get() == 1:
+        print('Haz ordenado una Hamburguesa')
+    elif x.get() == 2:
+        print('Haz ordenado un Hotdog')
+    else:
+        print('ERROR')
