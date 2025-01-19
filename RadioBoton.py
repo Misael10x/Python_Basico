f624252338693f7d53805021f088896025dd33d0
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-18 20:58:03 -0600

    Update RadioBoton.py

--- RadioBoton.py
+++ RadioBoton.py
@@ -19,3 +19,28 @@ def cargar_imagen(ruta, ancho, alto):
     return ImageTk.PhotoImage(imagen)
 
 windows = Tk()
+
+pizzaImage = cargar_imagen('pizza.png', 100, 100)
+hamburgerImage = cargar_imagen('hamburguesa.png', 100, 100)
+hotdogImage = cargar_imagen('hotdog.png', 100, 100)
+foodImage = [pizzaImage, hamburgerImage, hotdogImage]
+
+
+x = IntVar()
+
+for index in range(len(comida)):
+    radiobutton = Radiobutton(windows,
+                              text=comida[index],
+                              variable=x,
+                              value=index,
+                              padx=25,
+                              font=('Arial', 20),
+                              image=foodImage[index],
+                              compound='left',
+                              indicatoron=0,
+                              width=400,
+                              command=orden)
+    radiobutton.pack(anchor=W)
+
+
+windows.mainloop()
