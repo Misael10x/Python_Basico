261e668bbf17adccfc44e2e2a7f52c6aa61170b2
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-19 20:22:55 -0600

    Update Scale.py

--- Scale.py
+++ Scale.py
@@ -3,3 +3,8 @@ from tkinter import *
 def submit():
     print("La temperatura es: " + str(scale.get()) + " grados.")
 
+windows = Tk()
+
+hotImage = PhotoImage(file='h.png').subsample(5, 5)
+hotLabel = Label(image=hotImage)
+hotLabel.pack()
