58f728cff1a7772f372865ef19624caa96751ed2
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-19 20:23:05 -0600

    Update Scale.py

--- Scale.py
+++ Scale.py
@@ -8,3 +8,18 @@ windows = Tk()
 hotImage = PhotoImage(file='h.png').subsample(5, 5)
 hotLabel = Label(image=hotImage)
 hotLabel.pack()
+
+scale = Scale(windows,
+              from_=100,
+              to=0,
+              font='Arial',
+              length=500,
+              orient=VERTICAL,
+              tickinterval=10,
+              #showvalue=0,
+              resolution=5,
+              troughcolor='#ff6600',
+              fg='#ff1c00',
+              bg='#111111')
+scale.set(((scale['from'] - scale['to']) / 2) + scale['to'])
+scale.pack()
