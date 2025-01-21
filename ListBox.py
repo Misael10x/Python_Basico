cb56e5d5826c836617d99124459621b7c78c2abf
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-20 19:42:47 -0600

    Update ListBox.py

--- ListBox.py
+++ ListBox.py
@@ -5,3 +5,17 @@ def submit():
         food.insert(index,listbox.get(index))
         
         
+    print("Su orden es: ")
+    #print(listbox.get(listbox.curselection()))
+    for index in food:
+        print(index)
+
+def add():
+    listbox.insert(listbox.size(),entrybox.get())
+    listbox.config(height=listbox.size())
+
+def delete():
+    #listbox.delete(listbox.curselection())
+    for index in  reversed(listbox.curselection()):
+        listbox.delete(index)
+    listbox.config(height=listbox.size())
