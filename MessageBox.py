94c4934b284f1da4fd5148a79f7033c8fb64237f
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-23 20:25:16 -0600

    Update MessageBox.py

--- MessageBox.py
+++ MessageBox.py
@@ -1,2 +1,22 @@
 from tkinter import *
 from tkinter import messagebox
+
+def click():
+    #messagebox.showinfo(title='Hola Mundo', message='Saludos')
+    #while TRUE:
+         #messagebox.showwarning(title='PELIGRO', message='Tienes el virus T')
+     #messagebox.showerror(title='Error', message='Error, Capa 8')
+     #messagebox.askokcancel()
+     #if(messagebox.askretrycancel()):
+      #  print('Hola')
+    # else:
+     #   print('Hola2')
+    # messagebox.askyesno()
+     #pregunta = messagebox.askquestion(title='Pregunta', message='Te gusta el helado')
+     #if(pregunta == 'yes'):
+      #   print('Si te gusta el helado')
+     #else:
+      #     print('No te gusta el helado')
+      #var = (messagebox.askyesnocancel(title='Advertencia', message='Estas seguro de salir'))
+      #print(var)
+      
