5f45d9aea00ba720fa76e974b4434a8e105d010a
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-24 18:26:27 -0600

    Update ColorChooser.py

--- ColorChooser.py
+++ ColorChooser.py
@@ -5,3 +5,8 @@ def click():
    # color = colorchooser.askcolor()
    # colorHex = color[1]
     window.config(bg=colorchooser.askcolor()[1])
+
+window = Tk()
+window.geometry('420x420')
+boton = Button(text='Hac click en mi', command=click) 
+boton.pack()
