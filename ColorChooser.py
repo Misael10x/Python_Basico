3146a917c8c36df87d3555a4fd8f4268bd4618c8
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-24 18:24:09 -0600

    Update ColorChooser.py

--- ColorChooser.py
+++ ColorChooser.py
@@ -1,3 +1,7 @@
 from tkinter import *
 from tkinter import colorchooser
 
+def click():
+   # color = colorchooser.askcolor()
+   # colorHex = color[1]
+    window.config(bg=colorchooser.askcolor()[1])
