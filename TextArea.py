f41b05b647a146c257ea4b39039316412cc142a5
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-25 20:23:53 -0600

    Update TextArea.py

--- TextArea.py
+++ TextArea.py
@@ -3,3 +3,16 @@ from tkinter import *
 def Enviar():
     input = texto.get('1.0', END)
     print(input)
+
+
+window = Tk()
+
+texto = Text(window,
+             bg='#ffffe0',
+             font=('Inl Free', 25),
+             height=8,
+             width=20,
+             padx=20,
+             pady=20,
+             fg='red')
+texto.pack()
