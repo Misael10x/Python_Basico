0570312ec12a3668089a12550568d6480c44a725
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-26 19:42:48 -0600

    Update AbrirArchivo.py

--- AbrirArchivo.py
+++ AbrirArchivo.py
@@ -1,2 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
+
+def Abrir():
+    filepath = filedialog.askopenfilename(initialdir='/home/misael/Documentos/Parte/prueba.txt',
+                                          title='Abrir Archivo',
+                                          filetypes=(('Archivo de texto','*.txt'),('Todos los archivos', '*.*')))
