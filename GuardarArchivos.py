a55cc2a2f61061c296bb83720e619335127b39a6
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-27 18:52:41 -0600

    Update GuardarArchivos.py

--- GuardarArchivos.py
+++ GuardarArchivos.py
@@ -13,3 +13,15 @@ def Guardar():
                                            ])
     if archivo is None:
         return
+    archivoTexto = texto.get(1.0, END)
+    archivo.write(archivoTexto)
+    archivo.close()
+    
+window = Tk()
+
+boton = Button(window, text='Guardar', command=Guardar)
+boton.pack()
+texto = Text(window)
+texto.pack()
+
+window.mainloop()
