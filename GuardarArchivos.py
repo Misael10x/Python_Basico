a78b7b5ce558a8b2e5699ea51f62403a1930c5b9
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-27 18:50:33 -0600

    Update GuardarArchivos.py

--- GuardarArchivos.py
+++ GuardarArchivos.py
@@ -3,3 +3,11 @@
 from tkinter import *
 from tkinter import filedialog
 
+def Guardar():
+    archivo = filedialog.asksaveasfile(initialdir='/home/misael/Documentos/Parte',
+                                       defaultextension='.txt',
+                                       filetypes=[
+                                           ('ARchivos de texto', '.txt'),
+                                           ('HTML', '.html'),
+                                           ('Todos los acchivos','.*')
+                                           ])
