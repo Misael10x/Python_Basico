fabc73f872b3a2e0dd5a1a22ae7967acf8dcb4c4
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-28 20:24:14 -0600

    Update MenuNavegacion.py

--- MenuNavegacion.py
+++ MenuNavegacion.py
@@ -1,1 +1,12 @@
+from tkinter import *
 
+def abrir():
+    print('Abriendo archivo')
+def guardar():
+    print('Guardando......')
+def cortar():
+    print('Cortando......')
+def copiar():
+    print('Copiando......')
+def pegar():
+    print('Pegando......')
