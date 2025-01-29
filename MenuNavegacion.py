7f7d1a42a6b972cadb25f185d28c1c6fdeb0ba46
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-28 20:24:33 -0600

    Update MenuNavegacion.py

--- MenuNavegacion.py
+++ MenuNavegacion.py
@@ -10,3 +10,9 @@ def copiar():
     print('Copiando......')
 def pegar():
     print('Pegando......')
+
+window = Tk()
+
+openImage = PhotoImage(file='open.png').subsample(20,20)
+saveImage = PhotoImage(file='sa.png').subsample(25,25)
+exitImage = PhotoImage(file='ex.png').subsample(40,40)
