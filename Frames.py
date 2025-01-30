511236f393661864c5b7b0b45b08848c5947597a
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-29 19:13:49 -0600

    Update Frames.py

--- Frames.py
+++ Frames.py
@@ -4,3 +4,7 @@ window = Tk()
 
 marco = Frame(window, bg='pink', bd=5, relief=SUNKEN)
 marco.pack(side=TOP)
+
+Button(marco,text='W', font=('Consola', 25),width=3).pack(side=TOP)
+Button(marco,text='A', font=('Consola', 25),width=3).pack(side=LEFT)
+Button(marco,text='S', font=('Consola', 25),width=3).pack(side=LEFT)
