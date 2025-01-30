1c6714434bf5d4f1d0f89858acad1b2ed16c59eb
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-29 19:14:09 -0600

    Update Frames.py

--- Frames.py
+++ Frames.py
@@ -8,3 +8,6 @@ marco.pack(side=TOP)
 Button(marco,text='W', font=('Consola', 25),width=3).pack(side=TOP)
 Button(marco,text='A', font=('Consola', 25),width=3).pack(side=LEFT)
 Button(marco,text='S', font=('Consola', 25),width=3).pack(side=LEFT)
+Button(marco,text='D', font=('Consola', 25),width=3).pack(side=LEFT)
+
+window.mainloop()
