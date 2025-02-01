c085581ed3de74f00b766126e338064721f92929
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-31 22:08:29 -0600

    Update PestañasVentanas.py

--- PestañasVentanas.py
+++ PestañasVentanas.py
@@ -13,3 +13,10 @@ tab3 = Frame(noteboock)
 noteboock.add(tab1, text='Tab1')
 noteboock.add(tab2, text='Tab2')
 noteboock.add(tab3, text='Tab3')
+
+Label(tab1, text='Pestaña #1', width=50, height=25).pack()
+Label(tab2, text='Pestaña #2', width=50, height=25).pack()
+Label(tab3, text='Pestaña #3', width=50, height=25).pack()
+
+
+window.mainloop()
