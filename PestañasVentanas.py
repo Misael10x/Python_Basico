c51df211183e0c70cadb1f3ce584909c23f6b4b9
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-01-31 22:07:52 -0600

    Update PestañasVentanas.py

--- PestañasVentanas.py
+++ PestañasVentanas.py
@@ -5,3 +5,11 @@ window = Tk()
 
 noteboock = ttk.Notebook(window)
 noteboock.pack(expand=TRUE, fill='both')
+
+tab1 = Frame(noteboock)
+tab2 = Frame(noteboock)
+tab3 = Frame(noteboock)
+
+noteboock.add(tab1, text='Tab1')
+noteboock.add(tab2, text='Tab2')
+noteboock.add(tab3, text='Tab3')
