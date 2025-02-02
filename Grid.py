3d76517196ee5b335c320140a4838cb74e5b17b9
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-01 22:44:59 -0600

    Update Grid.py

--- Grid.py
+++ Grid.py
@@ -6,3 +6,9 @@ tituloLabel = Label(window,text='Ingresa tu informacion', font=('Arial',25)).gri
 
 nombreLabel = Label(window, text='Nombre: ',width=20,bg='red').grid(row=1,column=0)
 nombreEntry = Entry(window).grid(row=1,column=1)
+
+apelliodoLabel = Label(window, text='Apellido: ',width=20,bg='blue').grid(row=2,column=0)
+apellidoEntry = Entry(window).grid(row=2,column=1)
+
+emailLabel = Label(window, text='Email: ',width=20,bg='green').grid(row=3,column=0)
+emailEntry = Entry(window).grid(row=3,column=1)
