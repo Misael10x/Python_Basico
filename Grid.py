ed5fa318c99870bf7dba6a6c51968cde90ff9f48
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-01 22:44:47 -0600

    Update Grid.py

--- Grid.py
+++ Grid.py
@@ -1,3 +1,8 @@
 from tkinter import *
 
 window = Tk()
+
+tituloLabel = Label(window,text='Ingresa tu informacion', font=('Arial',25)).grid(row=0,column=0,columnspan=2)
+
+nombreLabel = Label(window, text='Nombre: ',width=20,bg='red').grid(row=1,column=0)
+nombreEntry = Entry(window).grid(row=1,column=1)
