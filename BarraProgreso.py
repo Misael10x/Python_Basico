21cf463720996862359fe2ca82f2b1f2855876c3
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-02 21:14:26 -0600

    Update BarraProgreso.py

--- BarraProgreso.py
+++ BarraProgreso.py
@@ -2,3 +2,15 @@ from tkinter import *
 from tkinter.ttk import *
 import time
 
+def start():
+    print('Descargando...')
+    GB = 100
+    dowload = 0
+    speed = 0.5
+    while(dowload<GB):
+        time.sleep(0.05)
+        bar['value']+= (speed/GB)*100
+        dowload+= speed
+        porcentaje.set(str(int((dowload/GB))*100)+'%')
+        texto.set(str(dowload)+'/'+str(GB)+ 'GB Completado')
+        window.update_idletasks()
