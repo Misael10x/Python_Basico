bd6ddbf0b65796670caf79cf09a322207ab5f1fc
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-03 19:31:09 -0600

    Update Canvas.py

--- Canvas.py
+++ Canvas.py
@@ -1,3 +1,11 @@
 from tkinter import *
 
 window = Tk()
+
+canvas = Canvas(window,height=500,width=500)
+#canvas.create_line(0,0,500,500,fill='blue',width=5)
+#canvas.create_line(0,500,500,0,fill='red',width=5)
+#canvas.create_rectangle(50,50,250,250,fill='purple')
+#puntos = [250,500,500,0,500]
+#canvas.create_polygon(puntos,fill='yellow',outline='black',width=5)
+#canvas.create_arc(0,0,500,500,fill='green',style=PIESLICE,start=270,extent=180)
