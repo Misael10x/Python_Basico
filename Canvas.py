45a941cf989433d29c7e29bd9624e082c9fbd82d
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-03 19:31:22 -0600

    Update Canvas.py

--- Canvas.py
+++ Canvas.py
@@ -9,3 +9,7 @@ canvas = Canvas(window,height=500,width=500)
 #puntos = [250,500,500,0,500]
 #canvas.create_polygon(puntos,fill='yellow',outline='black',width=5)
 #canvas.create_arc(0,0,500,500,fill='green',style=PIESLICE,start=270,extent=180)
+
+canvas.create_arc(0,0,500,500,fill='red',extent=180,width=10)
+canvas.create_arc(0,0,500,500,fill='white',extent=180,start=180,width=10)
+canvas.create_oval(190,190,310,310,fill='white',width=20)
