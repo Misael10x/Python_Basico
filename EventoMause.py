a1d04fab40d297c29592b0c1510313cc15146299
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-05 19:11:03 -0600

    Update EventoMause.py

--- EventoMause.py
+++ EventoMause.py
@@ -3,3 +3,10 @@ def hacer_algo(evento):
     print('Coordenadas: ' + str(evento.x) + ',' + str(evento.y))
 
 window = Tk()
+
+#window.bind('<Button-1>',hacer_algo)
+#window.bind('<Button-2>',hacer_algo)
+#window.bind('<Button-3>',hacer_algo)
+#window.bind('<ButtonRelease>',hacer_algo)
+#window.bind('<Enter>',hacer_algo)
+#window.bind('<Leave>',hacer_algo)
