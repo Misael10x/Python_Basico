f01d2fdb92aa4bf4fec934774d80de2969891da7
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-06 19:38:58 -0600

    Update Dado.py

--- Dado.py
+++ Dado.py
@@ -2,3 +2,9 @@
 import tkinter as tk
 from tkinter import messagebox
 import random
+
+# Función para lanzar el dado
+def lanzar_dado():
+    resultado = random.randint(1, 6)
+    resultado_label.config(text=f"Resultado: {resultado}")
+
