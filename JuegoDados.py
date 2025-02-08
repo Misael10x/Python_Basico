86193ae44c4a874e9c48ac5c6643db40f3e8dc42
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-07 20:44:29 -0600

    Update JuegoDados.py

--- JuegoDados.py
+++ JuegoDados.py
@@ -1,3 +1,8 @@
 import tkinter as tk
 import random
 
+def lanzar_dados():
+    dado1 = random.randint(1, 6)
+    dado2 = random.randint(1, 6)
+    suma = dado1 + dado2
+    resultado_label.config(text=f"Resultado: {dado1} + {dado2} = {suma}")
