70fe5ac1c8ffc896ec277645db565b1c5d14eb83
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-09 17:55:50 -0600

    Update SimuladorClima.py

--- SimuladorClima.py
+++ SimuladorClima.py
@@ -1,3 +1,8 @@
 import tkinter as tk
 import random
 
+def actualizar_clima():
+    climas = ["Soleado ☀️", "Nublado ☁️", "Lluvioso 🌧️", "Nevado ❄️"]
+    clima = random.choice(climas)
+    clima_label.config(text=f"Clima actual: {clima}")
+
