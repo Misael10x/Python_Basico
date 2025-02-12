540d5dce6af0bc287382e82e48f4f1e5f1adcee0
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-11 20:34:14 -0600

    Update RelojDigital.py

--- RelojDigital.py
+++ RelojDigital.py
@@ -5,3 +5,6 @@ def actualizar_hora():
     hora_actual = strftime("%H:%M:%S %p")
     etiqueta_hora.config(text=hora_actual)
     etiqueta_hora.after(1000, actualizar_hora) 
+window = tk.Tk()
+window.title("Reloj Digital")
+window.geometry("300x150")
