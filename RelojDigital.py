ea558e19a62f4ce1f46a97ee851260d12805a02e
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-11 20:34:04 -0600

    Update RelojDigital.py

--- RelojDigital.py
+++ RelojDigital.py
@@ -1,3 +1,7 @@
-
 import tkinter as tk
 from time import strftime
+
+def actualizar_hora():
+    hora_actual = strftime("%H:%M:%S %p")
+    etiqueta_hora.config(text=hora_actual)
+    etiqueta_hora.after(1000, actualizar_hora) 
