015d877296cf25d85af069eb82893d4b065d512d
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-11 20:34:49 -0600

    Update RelojDigital.py

--- RelojDigital.py
+++ RelojDigital.py
@@ -8,3 +8,10 @@ def actualizar_hora():
 window = tk.Tk()
 window.title("Reloj Digital")
 window.geometry("300x150")
+
+etiqueta_hora = tk.Label(window, font=("Arial", 40), background="black", foreground="white")
+etiqueta_hora.pack(expand=True)
+
+actualizar_hora()
+
+window.mainloop()
