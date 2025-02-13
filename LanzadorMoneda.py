38f066d6b8a8e89dc144c03b7e02c67f73774faa
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-12 19:55:17 -0600

    Update LanzadorMoneda.py

--- LanzadorMoneda.py
+++ LanzadorMoneda.py
@@ -4,3 +4,7 @@ import random
 def lanzar_moneda():
     resultado = random.choice(["Cara", "Cruz"])
     resultado_label.config(text=f"Resultado: {resultado}")
+
+window = tk.Tk()
+window.title("Lanzamiento de Moneda")
+window.geometry("300x150")
