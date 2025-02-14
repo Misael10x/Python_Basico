78be7bcf3c4a51853c76ca758c344e3a44cb960a
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-13 20:58:18 -0600

    Update RuletaColores.py

--- RuletaColores.py
+++ RuletaColores.py
@@ -1,2 +1,7 @@
 import tkinter as tk
 import random
+
+def girar_ruleta():
+    colores = ["Rojo", "Verde", "Azul", "Amarillo", "Naranja", "Morado", "Rosa", "Cyan"]
+    color_seleccionado = random.choice(colores)
+    resultado_label.config(text=f"Color seleccionado: {color_seleccionado}", bg=color_seleccionado.lower())
