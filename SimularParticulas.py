a409d5b8fabc9069e1843d495bf8ec373df2d05a
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-15 20:50:48 -0600

    Update SimularParticulas.py

--- SimularParticulas.py
+++ SimularParticulas.py
@@ -1,1 +1,8 @@
+import tkinter as tk
+import random
 
+# Configuración inicial
+ANCHO = 800
+ALTO = 600
+NUM_PARTICULAS = 50  # Número de partículas
+COLORES = ["red", "green", "blue", "yellow", "cyan", "magenta", "orange"]
