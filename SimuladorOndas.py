6f569c3cc42ae68e8a964491ea3c7cc6ca8409fc
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-17 20:32:16 -0600

    Update SimuladorOndas.py

--- SimuladorOndas.py
+++ SimuladorOndas.py
@@ -1,2 +1,9 @@
 import tkinter as tk
 import math
+
+# Configuración inicial
+ANCHO = 800
+ALTO = 400
+AMPLITUD = 80  # Altura de las ondas
+FRECUENCIA = 0.02  # Frecuencia de las ondas
+VELOCIDAD = 2  # Velocidad de desplazamiento de las ondas
