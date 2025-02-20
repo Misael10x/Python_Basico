7bda5adadb8f2d49c10b2005538a634c85ee456b
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-19 20:08:14 -0600

    Update SimuladorLluvia.py

--- SimuladorLluvia.py
+++ SimuladorLluvia.py
@@ -7,3 +7,16 @@ NUM_GOTAS = 100
 VELOCIDAD = 5
 
 class Gota:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.x = random.randint(0, ANCHO)
+        self.y = random.randint(-ALTO, 0)
+        self.longitud = random.randint(10, 20)
+        self.gota = canvas.create_line(self.x, self.y, self.x, self.y + self.longitud, fill="blue", width=2)
+
+    def caer(self):
+        self.y += VELOCIDAD
+        if self.y > ALTO:
+            self.y = random.randint(-ALTO, 0)
+            self.x = random.randint(0, ANCHO)
+        self.canvas.coords(self.gota, self.x, self.y, self.x, self.y + self.longitud)
