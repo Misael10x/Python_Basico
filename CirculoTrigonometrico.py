e1aad475a0ca531d76293518ce42f585f8d55408
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-20 18:55:42 -0600

    Update CirculoTrigonometrico.py

--- CirculoTrigonometrico.py
+++ CirculoTrigonometrico.py
@@ -1,5 +1,11 @@
-
 import matplotlib
 matplotlib.use('Agg')  # Usa el backend sin GUI
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Crear la figura y el eje
+fig, ax = plt.subplots(figsize=(6,6))
+
+# Dibujar el círculo
+circle = plt.Circle((0, 0), 1, color='blue', fill=False, linewidth=2)
+ax.add_patch(circle)
