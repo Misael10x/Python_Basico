05e1b7ece225b549b17f1f884f08fd319bd675d5
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-22 22:03:50 -0600

    Update SimuladorGalaxias.py

--- SimuladorGalaxias.py
+++ SimuladorGalaxias.py
@@ -5,4 +5,12 @@ import random
 ANCHO = 800
 ALTO = 600
 NUM_ESTRELLAS = 300
+VELOCIDAD = 0.02
 
+class Estrella:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.angulo = random.uniform(0, 2 * math.pi)
+        self.distancia = random.uniform(0, ANCHO // 2)
+        self.tamaño = random.uniform(1, 3)
+        self.estrella = canvas.create_oval(0, 0, self.tamaño, self.tamaño, fill="white")
