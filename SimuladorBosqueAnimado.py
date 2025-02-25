2afe25d4777aa19716a1089530b55cd7b15e898b
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-24 20:35:36 -0600

    Update SimuladorBosqueAnimado.py

--- SimuladorBosqueAnimado.py
+++ SimuladorBosqueAnimado.py
@@ -7,3 +7,10 @@ ALTO = 600
 NUM_ARBOLES = 10
 VELOCIDAD_VIENTO = 0.05
 AMPLITUD_VIENTO = 10
+
+class Arbol:
+    def _init_(self, canvas, x, y):
+        self.canvas = canvas
+        self.x = x
+        self.y = y
+        self.tronco = 
