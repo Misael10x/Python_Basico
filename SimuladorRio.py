a298643cab6d43fcdd458ef7d5941c9ef618972d
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-25 22:38:25 -0600

    Update SimuladorRio.py

--- SimuladorRio.py
+++ SimuladorRio.py
@@ -1,1 +1,11 @@
+import tkinter as tk
+import random
+import math
 
+# Configuración inicial
+ANCHO = 800
+ALTO = 600
+NUM_OLAS = 50
+AMPLITUD_OLAS = 20
+FRECUENCIA_OLAS = 0.02
+VELOCIDAD_OLAS = 0.05
