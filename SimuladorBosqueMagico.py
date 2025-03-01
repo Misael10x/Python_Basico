aa09ca1902edcccf7475e2c207f8d36055bd22e6
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-02-28 23:08:10 -0600

    Update SimuladorBosqueMagico.py

--- SimuladorBosqueMagico.py
+++ SimuladorBosqueMagico.py
@@ -6,3 +6,10 @@ ALTO = 600
 NUM_ARBOLES = 10
 NUM_LUCES = 20
 VELOCIDAD_PARPADEO = 200
+
+class Arbol:
+    def _init_(self, canvas, x, y):
+        self.canvas = canvas
+        self.x = x
+        self.y = y
+        self.tronco = canvas.create_rectangle(x - 10, y, x + 10, y + 100, fill="brown")
