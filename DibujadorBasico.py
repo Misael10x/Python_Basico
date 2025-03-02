1cd2c2e1ec360468c8f765ae52eef8d07d2876eb
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-01 20:34:30 -0600

    Update DibujadorBasico.py

--- DibujadorBasico.py
+++ DibujadorBasico.py
@@ -3,3 +3,8 @@ import tkinter as tk
 def comenzar_dibujo(evento):
     global x_previo, y_previo
     x_previo, y_previo = evento.x, evento.y
+
+def dibujar(evento):
+    global x_previo, y_previo
+    canvas.create_line(x_previo, y_previo, evento.x, evento.y, fill="black", width=2)
+    x_previo, y_previo = evento.x, evento.y
