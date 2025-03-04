a0368d7abb2b8132094075cd8f2d047a911cf6e0
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-03 20:32:55 -0600

    Update Texto_A_Voz.py

--- Texto_A_Voz.py
+++ Texto_A_Voz.py
@@ -4,3 +4,7 @@ def text_to_speech(text):
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
+
+if __name__ == "__main__":
+    texto = input("Escribe el texto que quieres convertir a voz: ")
+    text_to_speech(texto)
