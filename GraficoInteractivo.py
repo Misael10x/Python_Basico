c5c9ff56cdf2e7917ceada534c5c4a69d219df1b
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-05 19:57:45 -0600

    Update GraficoInteractivo.py

--- GraficoInteractivo.py
+++ GraficoInteractivo.py
@@ -6,3 +6,9 @@ x = np.random.rand(100)
 y = np.random.rand(100)
 sizes = np.random.rand(100) * 100
 colors = np.random.rand(100)
+
+fig = px.scatter(x=x, y=y, size=sizes, color=colors, 
+                 title="Gráfico Interactivo de Dispersión",
+                 labels={'x': 'Eje X', 'y': 'Eje Y'})
+
+fig.show()
