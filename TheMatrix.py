2f54bd16f23942a37f3f0909464f9f66aa394b85
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-06 22:09:31 -0600

    Update TheMatrix.py

--- TheMatrix.py
+++ TheMatrix.py
@@ -5,3 +5,7 @@ import os
 def matrix_effect(width=80, height=20, speed=0.05):
     chars = "01X#*@"
     columns = [0] * width
+    
+    while True:
+        os.system("cls" if os.name == "nt" else "clear")
+        for i in range(height):
