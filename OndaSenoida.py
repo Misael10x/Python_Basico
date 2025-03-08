5cb58f959c21ab1df4c6414c45837b197251c7a4
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-07 20:28:39 -0600

    Update OndaSenoida.py

--- OndaSenoida.py
+++ OndaSenoida.py
@@ -5,3 +5,7 @@ import matplotlib.animation as animation
 fig, ax = plt.subplots()
 x = np.linspace(0, 2 * np.pi, 100)
 line, = ax.plot(x, np.sin(x))
+
+def update(frame):
+    line.set_ydata(np.sin(x + frame / 10))
+    return line,
