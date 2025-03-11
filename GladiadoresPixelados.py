8f807682947487685af33beb8966db4fdc68f957
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-10 19:15:33 -0600

    Update GladiadoresPixelados.py

--- GladiadoresPixelados.py
+++ GladiadoresPixelados.py
@@ -6,3 +6,6 @@ player2 = "🤺"
 p1_health = 10
 p2_health = 10
 
+def display(p1, p2, h1, h2):
+    print(f"\n{player1} {'❤️' * h1}   VS   {'❤️' * h2} {player2}")
+    print(" " * random.randint(5, 15) + "⚔️")
