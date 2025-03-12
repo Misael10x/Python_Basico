299c08b77a60a60daf24ddaa668360226cf30c68
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-11 20:46:20 -0600

    Update OperacionesMitricesComplejas.py

--- OperacionesMitricesComplejas.py
+++ OperacionesMitricesComplejas.py
@@ -14,3 +14,24 @@ def operaciones_matriz(matriz):
     if matriz.shape[0] == matriz.shape[1]:
         det = np.linalg.det(matriz)
         print(f"\nDeterminante de la matriz: {det}")
+    
+    try:
+        inversa = np.linalg.inv(matriz)
+        print("\nInversa de la matriz:")
+        imprimir_matriz(inversa)
+    except np.linalg.LinAlgError:
+        print("\nLa matriz no es invertible.")
+    
+    transpuesta = np.transpose(matriz)
+    print("\nTranspuesta de la matriz:")
+    imprimir_matriz(transpuesta)
+
+def main():
+    filas = int(input("Introduce el número de filas: "))
+    columnas = int(input("Introduce el número de columnas: "))
+    
+    matriz_compleja = crear_matriz_compleja(filas, columnas)
+    operaciones_matriz(matriz_compleja)
+
+if __name__ == "__main__":
+    main()
