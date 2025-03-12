c28a23c8a2baf8097e0af14ecc02b10f86786870
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-11 20:45:07 -0600

    Update OperacionesMitricesComplejas.py

--- OperacionesMitricesComplejas.py
+++ OperacionesMitricesComplejas.py
@@ -2,3 +2,7 @@ import numpy as np
 
 def crear_matriz_compleja(filas, columnas):
     return np.random.rand(filas, columnas) + 1j * np.random.rand(filas, columnas)
+
+def imprimir_matriz(matriz):
+    for fila in matriz:
+        print(fila)
