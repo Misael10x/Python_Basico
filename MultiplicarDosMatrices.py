89c1f9adf3b783def4f9bcd5341f66c8ca64999e
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-15 22:48:54 -0600

    Update MultiplicarDosMatrices.py

--- MultiplicarDosMatrices.py
+++ MultiplicarDosMatrices.py
@@ -1,1 +1,7 @@
+import numpy as np
 
+def multiplicar_matrices_complejas():
+    n = int(input("Introduce el tamaño de la matriz (n x n): "))
+    
+    A = np.zeros((n, n), dtype=complex)
+    B = np.zeros((n, n), dtype=complex
