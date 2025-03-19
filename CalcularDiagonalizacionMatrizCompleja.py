00794f148d180c513384e832a832c675f0761d52
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-18 21:56:51 -0600

    Update CalcularDiagonalizacionMatrizCompleja.py

--- CalcularDiagonalizacionMatrizCompleja.py
+++ CalcularDiagonalizacionMatrizCompleja.py
@@ -11,3 +11,9 @@ def diagonalizar_matriz_compleja():
             real = float(input(f"Parte real de A[{i+1}][{j+1}]: "))
             imag = float(input(f"Parte imaginaria de A[{i+1}][{j+1}]: "))
             A[i, j] = complex(real, imag)
+
+    valores, vectores = np.linalg.eig(A)
+
+    D = np.diag(valores)
+    P = vectores
+    P_inv = np.linalg.inv(P)
