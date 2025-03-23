a5afca8aeb7ccc42ac8fc1ac3ed0a8d28ccb4048
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-22 21:24:59 -0600

    Update CalcularPotenciaMatriz.py

--- CalcularPotenciaMatriz.py
+++ CalcularPotenciaMatriz.py
@@ -1,6 +1,7 @@
-
 import numpy as np
 
 def potencia_matriz_compleja():
     n = int(input("Introduce el tamaño de la matriz cuadrada: "))
     p = int(input("Introduce la potencia a la que deseas elevar la matriz: "))
+
+    A = np.zeros((n, n), dtype=complex)
