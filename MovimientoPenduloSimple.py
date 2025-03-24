a5c849e83869763cf1892d1942c535add731c58d
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-23 21:12:28 -0600

    Update MovimientoPenduloSimple.py

--- MovimientoPenduloSimple.py
+++ MovimientoPenduloSimple.py
@@ -1,4 +1,10 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
+
+def pendulo(t, y, g, l):
+    theta, omega = y
+    dtheta_dt = omega
+    domega_dt = - (g / l) * np.sin(theta)
+    return [dtheta_dt, domega_dt]
+  
