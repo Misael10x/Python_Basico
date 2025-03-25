a6f8d3cc136c0b2438bfef02331808b28d52e4bc
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-24 19:37:30 -0600

    Update SoluciónEDOs.py

--- SoluciónEDOs.py
+++ SoluciónEDOs.py
@@ -6,3 +6,11 @@ G = 6.67430e-11
 dt = 24 * 3600  
 num_dias = 365  
 num_pasos = num_dias  
+
+class Planeta:
+    def __init__(self, nombre, masa, posicion, velocidad):
+        self.nombre = nombre
+        self.masa = masa
+        self.posicion = np.array(posicion, dtype=np.float64)
+        self.velocidad = np.array(velocidad, dtype=np.float64)
+        self.trayectoria = []
