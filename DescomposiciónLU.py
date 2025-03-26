c3110754810b1e403373c418f088db98a12d6701
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-25 21:56:33 -0600

    Update DescomposiciónLU.py

--- DescomposiciónLU.py
+++ DescomposiciónLU.py
@@ -4,3 +4,5 @@ from scipy.linalg import lu
 def descomposicion_lu():
     n = int(input("Introduce el tamaño de la matriz cuadrada: "))
     A = np.random.randint(1, 10, (n, n))
+    
+    P, L, U = lu(A)
