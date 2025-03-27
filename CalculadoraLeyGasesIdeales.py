890d91d255f0c0c88f96b887ed3e88af35ffac56
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-26 21:53:25 -0600

    Update CalculadoraLeyGasesIdeales.py

--- CalculadoraLeyGasesIdeales.py
+++ CalculadoraLeyGasesIdeales.py
@@ -1,3 +1,10 @@
 def calcular_gas_ideal():
     R = 0.0821  
     opcion = input("¿Qué deseas calcular? (P/V/n/T): ").strip().upper()
+
+    if opcion == "P":
+        n = float(input("Introduce la cantidad de gas (moles): "))
+        V = float(input("Introduce el volumen (litros): "))
+        T = float(input("Introduce la temperatura (Kelvin): "))
+        P = (n * R * T) / V
+        print(f"\nPresión: {P} atm")
