c9038391918f45acf1c30ac98b28e40985c5e973
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-27 21:33:17 -0600

    Update CalculadoraEnergíaCinética.py

--- CalculadoraEnergíaCinética.py
+++ CalculadoraEnergíaCinética.py
@@ -3,3 +3,6 @@ def energia_cinetica():
     v = float(input("Introduce la velocidad del objeto (m/s): "))
     Ec = 0.5 * m * v**2
     print(f"\nEnergía Cinética: {Ec} Joules")
+
+if __name__ == "__main__":
+    energia_cinetica()
