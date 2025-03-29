d2316cd363ca89d7681de4a1b1f0329cc7b99779
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-28 20:38:08 -0600

    Update CalculadoraLeyCoulomb.py

--- CalculadoraLeyCoulomb.py
+++ CalculadoraLeyCoulomb.py
@@ -4,3 +4,4 @@ def ley_de_coulomb():
     k = 8.99e9  
     q1 = float(input("Introduce la carga 1 (Coulombs): "))
     q2 = float(input("Introduce la carga 2 (Coulombs): "))
+    r = float(input("Introduce la distancia entre las cargas (metros): "))
