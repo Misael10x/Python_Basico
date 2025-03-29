f201fa3b9b373a56078b8c02107ab803bfe1a29d
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-28 20:38:18 -0600

    Update CalculadoraLeyCoulomb.py

--- CalculadoraLeyCoulomb.py
+++ CalculadoraLeyCoulomb.py
@@ -5,3 +5,6 @@ def ley_de_coulomb():
     q1 = float(input("Introduce la carga 1 (Coulombs): "))
     q2 = float(input("Introduce la carga 2 (Coulombs): "))
     r = float(input("Introduce la distancia entre las cargas (metros): "))
+
+    if r == 0:
+        print("\nLa distancia no puede ser cero.")
