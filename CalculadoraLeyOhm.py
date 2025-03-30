6b45ff1326a09b4a90be965a692614e5a6121c5d
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-03-29 20:08:26 -0600

    Update CalculadoraLeyOhm.py

--- CalculadoraLeyOhm.py
+++ CalculadoraLeyOhm.py
@@ -5,3 +5,8 @@ def ley_de_ohm():
         I = float(input("Introduce la corriente (A): "))
         R = float(input("Introduce la resistencia (Ω): "))
         V = I * R
+        print(f"\nVoltaje: {V} V")
+    elif opcion == "I":
+        V = float(input("Introduce el voltaje (V): "))
+        R = float(input("Introduce la resistencia (Ω): "))
+        I = V / R
