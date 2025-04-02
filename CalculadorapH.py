e8848659b5432c177636671da026ed656833f0aa
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-01 22:24:56 -0600

    Update CalculadorapH.py

--- CalculadorapH.py
+++ CalculadorapH.py
@@ -4,3 +4,4 @@ def calcular_pH():
     H_concentracion = float(input("Introduce la concentración de iones H⁺ (mol/L): "))
 
     if H_concentracion <= 0:
+        print("\nLa concentración debe ser mayor que cero.")
