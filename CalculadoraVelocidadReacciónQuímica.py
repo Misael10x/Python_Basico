28389234d9a1fa2580e17c6b9dcf480aadb4136f
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-03 19:54:11 -0600

    Update CalculadoraVelocidadReacciónQuímica.py

--- CalculadoraVelocidadReacciónQuímica.py
+++ CalculadoraVelocidadReacciónQuímica.py
@@ -1,3 +1,4 @@
 def ley_de_velocidad():
     k = float(input("Introduce la constante de velocidad (L/mol·s): "))
     A = float(input("Introduce la concentración inicial de A (mol/L): "))
+    t = float(input("Introduce el tiempo (segundos): "))
