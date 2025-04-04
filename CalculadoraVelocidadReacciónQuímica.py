443471fdc31b85dd6c5b220392c3bda00aa33a60
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-03 19:58:10 -0600

    Update CalculadoraVelocidadReacciónQuímica.py

--- CalculadoraVelocidadReacciónQuímica.py
+++ CalculadoraVelocidadReacciónQuímica.py
@@ -6,3 +6,6 @@ def ley_de_velocidad():
     # Ley de velocidad para una reacción de primer orden
     velocidad = k * A
     print(f"\nVelocidad de la reacción: {velocidad:.2e} mol/L·s")
+
+if __name__ == "__main__":
+    ley_de_velocidad()
