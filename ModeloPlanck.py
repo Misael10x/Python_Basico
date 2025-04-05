9922f9c66874856211ca34f4ef2e92a10b832986
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-04 20:33:14 -0600

    Update ModeloPlanck.py

--- ModeloPlanck.py
+++ ModeloPlanck.py
@@ -2,3 +2,4 @@ def energia_foton():
     h = 6.626e-34  # Constante de Planck en J·s
     f = float(input("Introduce la frecuencia del fotón (Hz): "))
     E = h * f
+    print(f"\nEnergía del fotón: {E:.3e} Joules")
