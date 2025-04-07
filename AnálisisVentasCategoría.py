516b12e5f39d6fefc03ec0f3133d603dae95086a
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-06 20:31:24 -0600

    Update AnálisisVentasCategoría.py

--- AnálisisVentasCategoría.py
+++ AnálisisVentasCategoría.py
@@ -10,3 +10,14 @@ def analizar_ventas():
 
     print("\nVentas totales por categoría:")
     print(df.groupby("Categoría")["Ventas"].sum())
+
+    df.groupby("Categoría")["Ventas"].sum().plot(kind="bar", color="skyblue")
+    plt.title("Ventas por Categoría")
+    plt.xlabel("Categoría")
+    plt.ylabel("Ventas")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    analizar_ventas()
