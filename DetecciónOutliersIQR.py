2c0c20a0492d705ec33dd6015ea1f3338326469b
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-07 20:26:34 -0600

    Update DetecciónOutliersIQR.py

--- DetecciónOutliersIQR.py
+++ DetecciónOutliersIQR.py
@@ -4,3 +4,7 @@ def detectar_outliers():
     archivo = input("Nombre del archivo CSV: ")
     columna = input("Nombre de la columna numérica a analizar: ")
     df = pd.read_csv(archivo)
+
+    Q1 = df[columna].quantile(0.25)
+    Q3 = df[columna].quantile(0.75)
+    IQR = Q3 - Q1
