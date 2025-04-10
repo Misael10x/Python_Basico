42e77f4c3e30cb919287af4628aa79c08798a5a3
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-09 20:57:56 -0600

    Update AnálisisSentimientosTextos.py

--- AnálisisSentimientosTextos.py
+++ AnálisisSentimientosTextos.py
@@ -5,3 +5,6 @@ def analisis_sentimientos():
     archivo = input("Archivo CSV: ")
     columna = input("Nombre de la columna con texto: ")
     df = pd.read_csv(archivo)
+
+    df["Polaridad"] = df[columna].apply(lambda x: TextBlob(str(x)).sentiment.polarity)
+    df["Sentimiento"] = df["Polaridad"].apply(lambda p: "Positivo" if p > 0 else "Negativo" if p < 0 else "Neutro")
