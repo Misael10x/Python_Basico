b34dcad0e470e6c9c1e864b3744955969f09fe3e
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-10 20:16:02 -0600

    Update DistribuciónTemporalDatosMes.py

--- DistribuciónTemporalDatosMes.py
+++ DistribuciónTemporalDatosMes.py
@@ -1,3 +1,7 @@
-
 import pandas as pd
 import matplotlib.pyplot as plt
+
+def analisis_por_mes():
+    archivo = input("Archivo CSV: ")
+    columna_fecha = input("Nombre de la columna con fechas (YYYY-MM-DD): ")
+    df = pd.read_csv(archivo)
