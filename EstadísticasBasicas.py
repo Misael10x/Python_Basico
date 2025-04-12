878cfd65b24660854be138dcb9212919eb12d186
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-11 20:24:12 -0600

    Update EstadísticasBasicas.py

--- EstadísticasBasicas.py
+++ EstadísticasBasicas.py
@@ -3,3 +3,5 @@ from scipy import stats
 
 def estadisticas_basicas():
     archivo = input("Nombre del archivo CSV: ")
+    df = pd.read_csv(archivo)
+    datos_numericos = df.select_dtypes(include='number')
