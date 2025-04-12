0f4e75d85cd3817818af9adab776c00157cce926
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-11 20:24:46 -0600

    Update EstadísticasBasicas.py

--- EstadísticasBasicas.py
+++ EstadísticasBasicas.py
@@ -9,3 +9,9 @@ def estadisticas_basicas():
     for columna in datos_numericos.columns:
         print(f"\nColumna: {columna}")
         print(f"Media: {datos_numericos[columna].mean()}")
+        print(f"Mediana: {datos_numericos[columna].median()}")
+        print(f"Moda: {datos_numericos[columna].mode().values}")
+        print(f"Desviación estándar: {datos_numericos[columna].std()}")
+        print(f"Varianza: {datos_numericos[columna].var()}")
+        print(f"Mínimo: {datos_numericos[columna].min()}")
+        print(f"Máximo: {datos_numericos[columna].max()}")
