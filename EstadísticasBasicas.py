1bd51e8b91c97cd59e7a5e7c147f05e7cfdb7282
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-11 20:24:23 -0600

    Update EstadísticasBasicas.py

--- EstadísticasBasicas.py
+++ EstadísticasBasicas.py
@@ -5,3 +5,7 @@ def estadisticas_basicas():
     archivo = input("Nombre del archivo CSV: ")
     df = pd.read_csv(archivo)
     datos_numericos = df.select_dtypes(include='number')
+
+    for columna in datos_numericos.columns:
+        print(f"\nColumna: {columna}")
+        print(f"Media: {datos_numericos[columna].mean()}")
