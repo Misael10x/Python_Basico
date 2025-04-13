07284a87fe94c5bbbdd94c7fb06a682e67b00e0a
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-12 20:25:39 -0600

    Update VisualizacionCorrelacionesDatosNumericos.py

--- VisualizacionCorrelacionesDatosNumericos.py
+++ VisualizacionCorrelacionesDatosNumericos.py
@@ -6,3 +6,8 @@ def mapa_correlacion():
     archivo = input("Nombre del archivo CSV: ")
     df = pd.read_csv(archivo)
     datos_numericos = df.select_dtypes(include='number')
+    
+    correlaciones = datos_numericos.corr()
+
+    print("\nMatriz de correlación:")
+    print(correlaciones)
