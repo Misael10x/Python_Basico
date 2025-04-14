17d974e70070bdb4781321cfe7c992f6355a504a
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-13 19:43:10 -0600

    Update NormalizacionDatosNumericosMin-MaxScaling.py

--- NormalizacionDatosNumericosMin-MaxScaling.py
+++ NormalizacionDatosNumericosMin-MaxScaling.py
@@ -3,3 +3,5 @@ from sklearn.preprocessing import MinMaxScaler
 
 def normalizar_datos():
     archivo = input("Archivo CSV: ")
+    df = pd.read_csv(archivo)
+    numericas = df.select_dtypes(include='number')
