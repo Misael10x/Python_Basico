d850a4e729666dd11f827a0750ff490c0514a952
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-13 19:43:00 -0600

    Update NormalizacionDatosNumericosMin-MaxScaling.py

--- NormalizacionDatosNumericosMin-MaxScaling.py
+++ NormalizacionDatosNumericosMin-MaxScaling.py
@@ -1,5 +1,5 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-def normalizar_datos():import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+def normalizar_datos():
+    archivo = input("Archivo CSV: ")
