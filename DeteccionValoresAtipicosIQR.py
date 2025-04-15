a04d5b3b87831280437f906ac455144ab5b20b21
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-14 20:35:22 -0600

    Update DeteccionValoresAtipicosIQR.py

--- DeteccionValoresAtipicosIQR.py
+++ DeteccionValoresAtipicosIQR.py
@@ -4,3 +4,8 @@ def detectar_outliers():
     archivo = input("Archivo CSV: ")
     df = pd.read_csv(archivo)
     numericas = df.select_dtypes(include='number')
+
+    for columna in numericas.columns:
+        q1 = numericas[columna].quantile(0.25)
+        q3 = numericas[columna].quantile(0.75)
+        iqr = q3 - q1
