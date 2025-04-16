1bf45f0d1d5c15e67cfcce4e45af412b8ce44f49
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-15 19:47:03 -0600

    Update PrediccionRegresionLinealSimple.py

--- PrediccionRegresionLinealSimple.py
+++ PrediccionRegresionLinealSimple.py
@@ -3,3 +3,5 @@ from sklearn.linear_model import LinearRegression
 
 def regresion_lineal():
     archivo = input("Archivo CSV: ")
+    columna_x = input("Columna X: ")
+    columna_y = input("Columna Y: ")
