4b0ed1e7e2a497b6270ae083ad6b8eaada9e75f7
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-16 20:55:49 -0600

    Update ClasificacionTextoNaiveBayes.py

--- ClasificacionTextoNaiveBayes.py
+++ ClasificacionTextoNaiveBayes.py
@@ -2,3 +2,4 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
