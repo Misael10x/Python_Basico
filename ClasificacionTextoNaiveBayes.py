5565f6e320098b13a169e420fb99d14f366fceec
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-16 20:56:53 -0600

    Update ClasificacionTextoNaiveBayes.py

--- ClasificacionTextoNaiveBayes.py
+++ ClasificacionTextoNaiveBayes.py
@@ -3,3 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
+
+def clasificar_texto():
+    archivo = input("Archivo CSV: ")
+    columna_texto = input("Nombre de la columna de texto: ")
+    columna_etiqueta = input("Nombre de la columna de etiquetas: ")
