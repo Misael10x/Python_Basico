d7f959bebbc083f2716b05a08503f1764c0ff329
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-17 19:23:26 -0600

    Update CalculadoraGeometricaAreaPerimetro.py

--- CalculadoraGeometricaAreaPerimetro.py
+++ CalculadoraGeometricaAreaPerimetro.py
@@ -3,3 +3,8 @@ import math
 def figuras_geometricas():
     print("Figuras disponibles: círculo, triángulo, cuadrado, rectángulo")
     figura = input("Elige una figura: ").lower()
+
+    if figura == "círculo":
+        r = float(input("Radio: "))
+        area = math.pi * r ** 2
+        perimetro = 2 * math.pi * r
