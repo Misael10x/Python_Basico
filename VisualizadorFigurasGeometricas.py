be21a9d40d99614c4ec76c2279aae9eb27c10155
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-18 21:00:37 -0600

    Update VisualizadorFigurasGeometricas.py

--- VisualizadorFigurasGeometricas.py
+++ VisualizadorFigurasGeometricas.py
@@ -10,3 +10,22 @@ def dibujar_figura():
         for _ in range(4):
             t.forward(lado)
             t.right(90)
+
+    elif figura == "triángulo":
+        lado = int(input("Lado: "))
+        for _ in range(3):
+            t.forward(lado)
+            t.left(120)
+
+    elif figura == "círculo":
+        radio = int(input("Radio: "))
+        t.circle(radio)
+
+    else:
+        print("Figura no válida.")
+        turtle.bye()
+
+    turtle.done()
+
+if __name__ == "__main__":
+    dibujar_figura()
