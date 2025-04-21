ae649f51299ce23552d4f3262a161731aaa71cee
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-20 21:11:01 -0600

    Update Visualizador2DFigurasGeometricas.py

--- Visualizador2DFigurasGeometricas.py
+++ Visualizador2DFigurasGeometricas.py
@@ -19,3 +19,13 @@ def dibujar_figuras():
     # Rectángulo
     rectangulo = plt.Rectangle((6, 0.5), 1.5, 1, edgecolor='purple', fill=False)
     ax.add_patch(rectangulo)
+
+    ax.set_xlim(0, 8)
+    ax.set_ylim(0, 3)
+    ax.set_aspect('equal')
+    plt.grid(True)
+    plt.title("Figuras Geométricas 2D")
+    plt.show()
+
+if __name__ == "__main__":
+    dibujar_figuras()
