8a61bf6119b69f1defe8bbf1980e7c73e68ec469
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-20 21:08:27 -0600

    Update Visualizador2DFigurasGeometricas.py

--- Visualizador2DFigurasGeometricas.py
+++ Visualizador2DFigurasGeometricas.py
@@ -3,3 +3,7 @@ import numpy as np
 
 def dibujar_figuras():
     fig, ax = plt.subplots()
+
+    # Círculo
+    circulo = plt.Circle((1, 1), 0.5, color='blue', fill=False)
+    ax.add_patch(circulo)
