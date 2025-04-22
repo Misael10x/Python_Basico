83db62555d4d04789c4c7db45d94676135705861
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-21 19:12:15 -0600

    Update SistemaBasicoRegistroeInicioSesionArchivosJSON.py

--- SistemaBasicoRegistroeInicioSesionArchivosJSON.py
+++ SistemaBasicoRegistroeInicioSesionArchivosJSON.py
@@ -6,3 +6,7 @@ def cargar_usuarios():
         return {}
     with open("usuarios.json", "r") as f:
         return json.load(f)
+
+def guardar_usuarios(usuarios):
+    with open("usuarios.json", "w") as f:
+        json.dump(usuarios, f)
