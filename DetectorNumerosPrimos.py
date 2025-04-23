eb96cf4dd2812e6a3346eeb2544f9c9af2854bcd
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-22 21:23:06 -0600

    Update DetectorNumerosPrimos.py

--- DetectorNumerosPrimos.py
+++ DetectorNumerosPrimos.py
@@ -6,3 +6,16 @@ def es_primo(n):
     if n % 2 == 0:
         return False
     for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+def main():
+    numero = int(input("Ingresa un número: "))
+    if es_primo(numero):
+        print(f"{numero} es primo.")
+    else:
+        print(f"{numero} no es primo.")
+
+if __name__ == "__main__":
+    main()
