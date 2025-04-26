391d27217326db4082018832bef8d0a9c56e1212
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-25 20:59:42 -0600

    Update ConversoristemasNumericos.py

--- ConversoristemasNumericos.py
+++ ConversoristemasNumericos.py
@@ -1,3 +1,11 @@
 def convertir(numero):
     print("Binario:     ", bin(numero)[2:])
     print("Octal:       ", oct(numero)[2:])
+    print("Hexadecimal: ", hex(numero)[2:].upper())
+
+def main():
+    n = int(input("Ingresa un número decimal: "))
+    convertir(n)
+
+if __name__ == "__main__":
+    main()
