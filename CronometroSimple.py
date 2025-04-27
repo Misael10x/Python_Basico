722752b40e6cd12a15b42bd1961f811252234446
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-26 22:18:14 -0600

    Update CronometroSimple.py

--- CronometroSimple.py
+++ CronometroSimple.py
@@ -3,3 +3,5 @@ import time
 def cronometro(segundos):
     for i in range(segundos, 0, -1):
         print(f"Tiempo restante: {i} segundos", end="\r", flush=True)
+        time.sleep(1)
+    print("\n¡Tiempo terminado!")
