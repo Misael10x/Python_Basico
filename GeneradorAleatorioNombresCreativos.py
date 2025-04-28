5cfbcf6e19a1d8824a63d89e134cb21f34ce3adb
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-27 19:39:29 -0600

    Update GeneradorAleatorioNombresCreativos.py

--- GeneradorAleatorioNombresCreativos.py
+++ GeneradorAleatorioNombresCreativos.py
@@ -2,3 +2,4 @@ import random
 
 def generar_nombre():
     adjetivos = ["Rápido", "Feroz", "Brillante", "Oscuro", "Radiante", "Misterioso"]
+    sustantivos = ["León", "Águila", "Dragón", "Lobo", "Fénix", "Tigre"]
