66c871549da28411620af6d9e3a09b68c87d1e7c
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-28 19:46:50 -0600

    Update SimuladorVotacionesAleatorias.py

--- SimuladorVotacionesAleatorias.py
+++ SimuladorVotacionesAleatorias.py
@@ -1,3 +1,6 @@
     opciones = ["Opción A", "Opción B", "Opción C"]
     votos = {opcion: 0 for opcion in opciones}
     cantidad = int(input("¿Cuántos votos quieres simular? "))
+    for _ in range(cantidad):
+        voto = choice(opciones)
+        votos[voto] += 1
