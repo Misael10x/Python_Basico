8db3e87198769c2515ca2157557a6d0b52d450c1
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-29 20:36:19 -0600

    Update CalculadoraVolumenAreaSuperficial3D.py

--- CalculadoraVolumenAreaSuperficial3D.py
+++ CalculadoraVolumenAreaSuperficial3D.py
@@ -2,3 +2,7 @@ def esfera(r):
     volumen = (4/3) * math.pi * r**3
     area = 4 * math.pi * r**2
     return volumen, area
+def cubo(lado):
+    volumen = lado ** 3
+    area = 6 * lado ** 2
+    return volumen, area
