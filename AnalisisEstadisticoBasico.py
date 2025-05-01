2dc27baeae74dcb6d21aff250a2575a78c03b5b2
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-04-30 20:26:46 -0600

    Update AnalisisEstadisticoBasico.py

--- AnalisisEstadisticoBasico.py
+++ AnalisisEstadisticoBasico.py
@@ -3,3 +3,6 @@ import statistics
 def analisis(lista):
     media = statistics.mean(lista)
     mediana = statistics.median(lista)
+    desviacion = statistics.stdev(lista) if len(lista) > 1 else 0
+    minimo = min(lista)
+    maximo = max(lista)
