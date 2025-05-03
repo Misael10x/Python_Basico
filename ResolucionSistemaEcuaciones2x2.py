5efafe26b412dc03358ec152edb19767763ab350
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-02 20:53:40 -0600

    Update ResolucionSistemaEcuaciones2x2.py

--- ResolucionSistemaEcuaciones2x2.py
+++ ResolucionSistemaEcuaciones2x2.py
@@ -3,3 +3,6 @@ A = np.array([[2, 3],
               [1, -1]])
 B = np.array([8, 2])
 X = np.linalg.solve(A, B)
+print("Solución del sistema:")
+print("x =", round(X[0], 2))
+print("y =", round(X[1], 2))
