27cccde20dc56e2eb569a8b0e71a15344bea4e50
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-02 20:58:02 -0600

    Update ResolucionSistemaEcuaciones2x2.py

--- ResolucionSistemaEcuaciones2x2.py
+++ ResolucionSistemaEcuaciones2x2.py
@@ -1,10 +1,15 @@
 import numpy as np
+
 A = np.array([[2, 3],
               [1, -1]])
+
 B = np.array([8, 2])
+
 X = np.linalg.solve(A, B)
+
 print("Solución del sistema:")
 print("x =", round(X[0], 2))
 print("y =", round(X[1], 2))
+
 verif = np.dot(A, X)
 print("Verificación Ax =", verif)
