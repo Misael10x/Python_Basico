aba818fd520153c3771b5725c23aac7b72391f13
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-04 19:55:17 -0600

    Update CalculadoraEstadistica.py

--- CalculadoraEstadistica.py
+++ CalculadoraEstadistica.py
@@ -7,3 +7,8 @@ media = statistics.mean(numeros)
 mediana = statistics.median(numeros)
 moda = statistics.mode(numeros)
 desviacion = statistics.stdev(numeros)
+
+print("Media:", round(media, 2))
+print("Mediana:", round(mediana, 2))
+print("Moda:", moda)
+print("Desviación estándar:", round(desviacion, 2))
