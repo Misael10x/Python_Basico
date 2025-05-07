96e62c83489f843454e76aed000645482e371f94
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-06 20:30:33 -0600

    Update ConversorQuimicoMolesMasaPartículas.py

--- ConversorQuimicoMolesMasaPartículas.py
+++ ConversorQuimicoMolesMasaPartículas.py
@@ -4,3 +4,4 @@ op = input("¿Qué deseas convertir? (masa/moles/particulas): ").lower()
 
 if op == "masa":
     masa = float(input("Ingresa la masa en gramos: "))
+    pm = float(input("Ingresa el peso molecular (g/mol): "))
