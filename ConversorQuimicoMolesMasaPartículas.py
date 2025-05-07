89a9adef103bff4a6e149105182d1fc50e3d3545
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-06 20:31:28 -0600

    Update ConversorQuimicoMolesMasaPartículas.py

--- ConversorQuimicoMolesMasaPartículas.py
+++ ConversorQuimicoMolesMasaPartículas.py
@@ -7,3 +7,20 @@ if op == "masa":
     pm = float(input("Ingresa el peso molecular (g/mol): "))
     moles = masa / pm
     particulas = moles * NA
+elif op == "moles":
+    moles = float(input("Ingresa la cantidad de moles: "))
+    pm = float(input("Ingresa el peso molecular (g/mol): "))
+    masa = moles * pm
+    particulas = moles * NA
+elif op == "particulas":
+    particulas = float(input("Ingresa el número de partículas: "))
+    moles = particulas / NA
+    pm = float(input("Ingresa el peso molecular (g/mol): "))
+    masa = moles * pm
+else:
+    print("Opción no válida")
+    exit()
+
+print("Moles:", round(moles, 4))
+print("Masa (g):", round(masa, 4))
+print("Partículas:", format(particulas, ".2e"))
