c70c41eb2f85fd8f1348bf886a294cdffdb9f9e5
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-09 21:17:03 -0600

    Update GeneradorAleatorioFrasesMotivacionales.py

--- GeneradorAleatorioFrasesMotivacionales.py
+++ GeneradorAleatorioFrasesMotivacionales.py
@@ -6,3 +6,8 @@ frases = [
     "Cree en ti, incluso cuando nadie más lo haga.",
     "El esfuerzo de hoy es el éxito de mañana.",
     "Fracasar es parte del camino hacia el éxito.",
+    "No te rindas, aún estás a tiempo de cambiar tu historia."
+]
+
+print("Frase del día:\n")
+print(random.choice(frases))
