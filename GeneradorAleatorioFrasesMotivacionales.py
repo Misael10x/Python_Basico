564ee9062656b9899579b5156bb9f26a1a6a1dde
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-09 21:16:55 -0600

    Update GeneradorAleatorioFrasesMotivacionales.py

--- GeneradorAleatorioFrasesMotivacionales.py
+++ GeneradorAleatorioFrasesMotivacionales.py
@@ -4,3 +4,5 @@ frases = [
     "Sigue adelante, lo mejor está por venir.",
     "Nunca es tarde para comenzar de nuevo.",
     "Cree en ti, incluso cuando nadie más lo haga.",
+    "El esfuerzo de hoy es el éxito de mañana.",
+    "Fracasar es parte del camino hacia el éxito.",
