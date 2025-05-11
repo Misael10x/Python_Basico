e0de3ae01aab1fef4556710d19c0c5d25fde97e1
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-10 20:58:30 -0600

    Update ConversorTemperaturas.py

--- ConversorTemperaturas.py
+++ ConversorTemperaturas.py
@@ -10,3 +10,10 @@ elif op == "F":
 elif op == "K":
     c = valor - 273.15
     f = c * 9/5 + 32
+else:
+    print("Opción inválida")
+    exit()
+
+print("Celsius:", round(c, 2) if op != "C" else round(valor, 2))
+print("Fahrenheit:", round(f, 2) if op != "F" else round(valor, 2))
+print("Kelvin:", round(k, 2) if op != "K" else round(valor, 2))
