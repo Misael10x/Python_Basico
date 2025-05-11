dee43cbccdd6186d8c0d3ae9b3d165bdc127ef41
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-10 20:58:18 -0600

    Update ConversorTemperaturas.py

--- ConversorTemperaturas.py
+++ ConversorTemperaturas.py
@@ -4,3 +4,9 @@ valor = float(input("Ingresa el valor de temperatura: "))
 if op == "C":
     f = valor * 9/5 + 32
     k = valor + 273.15
+elif op == "F":
+    c = (valor - 32) * 5/9
+    k = c + 273.15
+elif op == "K":
+    c = valor - 273.15
+    f = c * 9/5 + 32
