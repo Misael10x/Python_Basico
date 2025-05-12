19eaa2d1dfc697787f87b43db95878bd7643ae88
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-11 20:10:39 -0600

    Update ValidadorContrasenasSeguras.py

--- ValidadorContrasenasSeguras.py
+++ ValidadorContrasenasSeguras.py
@@ -3,4 +3,5 @@ import re
 contraseña = input("Ingresa tu contraseña: ")
 
 longitud = len(contraseña) >= 8
-mayuscula = re.search(r"[A-Z]", contraseña)s
+mayuscula = re.search(r"[A-Z]", contraseña)
+minuscula = re.search(r"[a-z]", contraseña)
