859720ce9ae32efd6ad6eec3937dca1d86bd218a
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-11 20:11:33 -0600

    Update ValidadorContrasenasSeguras.py

--- ValidadorContrasenasSeguras.py
+++ ValidadorContrasenasSeguras.py
@@ -7,3 +7,8 @@ mayuscula = re.search(r"[A-Z]", contraseña)
 minuscula = re.search(r"[a-z]", contraseña)
 digito = re.search(r"[0-9]", contraseña)
 especial = re.search(r"[!@#$%^&*(),.?\":{}|<>]", contraseña)
+
+if all([longitud, mayuscula, minuscula, digito, especial]):
+    print("Contraseña segura.")
+else:
+    print("Contraseña débil. Debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial.")
