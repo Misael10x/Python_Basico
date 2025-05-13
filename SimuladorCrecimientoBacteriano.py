8592a73f4688a172f2d6fd076d1c4dd879e282b1
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-12 20:25:48 -0600

    Update SimuladorCrecimientoBacteriano.py

--- SimuladorCrecimientoBacteriano.py
+++ SimuladorCrecimientoBacteriano.py
@@ -3,3 +3,5 @@ import math
 n0 = float(input("Cantidad inicial de bacterias: "))
 r = float(input("Tasa de crecimiento por hora (ej. 0.5 para 50%): "))
 t = float(input("Horas transcurridas: "))
+
+nt = n0 * math.exp(r * t)
