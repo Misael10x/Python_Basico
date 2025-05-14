5ebdea252b102a2f6a9f51b2647cfc3a7b4ab282
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-13 21:05:41 -0600

    Update GeneradorPiramideNumerica.py

--- GeneradorPiramideNumerica.py
+++ GeneradorPiramideNumerica.py
@@ -5,4 +5,6 @@ for i in range(1, n + 1):
         print(" ", end=" ")
     for k in range(1, i + 1):
         print(k, end=" ")
-    for l in range(i - 1, 0,
+    for l in range(i - 1, 0, -1):
+        print(l, end=" ")
+    print()
