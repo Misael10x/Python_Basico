c32284655f24d5a9c33521a9a499d5a08f1bf73e
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-14 20:54:48 -0600

    Update SimuladorDecaimientoRadiactivo.py

--- SimuladorDecaimientoRadiactivo.py
+++ SimuladorDecaimientoRadiactivo.py
@@ -3,3 +3,5 @@ import math
 n0 = float(input("Cantidad inicial de núcleos radiactivos: "))
 t = float(input("Tiempo transcurrido (en años): "))
 hl = float(input("Vida media del material (en años): "))
+
+nt = n0 * (0.5) ** (t / hl)
