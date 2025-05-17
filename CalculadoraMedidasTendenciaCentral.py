e7d166c6926ac6769208e5e5363007430ff3be6a
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-16 20:50:58 -0600

    Update CalculadoraMedidasTendenciaCentral.py

--- CalculadoraMedidasTendenciaCentral.py
+++ CalculadoraMedidasTendenciaCentral.py
@@ -4,3 +4,4 @@ datos = list(map(float, input("Ingresa números separados por espacio: ").split(
 
 print("Media:", round(mean(datos), 2))
 print("Mediana:", round(median(datos), 2))
+print("Moda:", mode(datos))
