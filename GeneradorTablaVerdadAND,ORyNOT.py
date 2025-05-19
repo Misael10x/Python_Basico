e7ebc19de73524be755f6992d5a7774892aa00fb
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-18 21:28:42 -0600

    Update GeneradorTablaVerdadAND,ORyNOT.py

--- GeneradorTablaVerdadAND,ORyNOT.py
+++ GeneradorTablaVerdadAND,ORyNOT.py
@@ -6,3 +6,5 @@ for a in valores:
     for b in valores:
         and_op = a & b
         or_op = a | b
+        not_a = int(not a)
+        print(f" {a} | {b} |    {and_op}     |   {or_op}   |   {not_a} ")
