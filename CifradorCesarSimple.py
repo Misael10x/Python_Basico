46e1d0a12510dd8dd522708c67a7660a90eb3099
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-20 20:44:09 -0600

    Update CifradorCesarSimple.py

--- CifradorCesarSimple.py
+++ CifradorCesarSimple.py
@@ -3,3 +3,5 @@ def cifrar_cesar(texto, desplazamiento):
     for char in texto:
         if char.isalpha():
             base = ord('A') if char.isupper() else ord('a')
+            nueva_letra = chr((ord(char) - base + desplazamiento) % 26 + base)
+            resultado += nueva_letra
