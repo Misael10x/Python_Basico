61d90857e9836893d6b3448c7b7b203a2e884132
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-21 22:01:23 -0600

    Update GeneradorTrianguloPascal.py

--- GeneradorTrianguloPascal.py
+++ GeneradorTrianguloPascal.py
@@ -5,3 +5,7 @@ def triangulo_pascal(n):
         for k in range(fila + 1):
             print(f"{numero} ", end="")
             numero = numero * (fila - k) // (k + 1)
+        print()
+
+filas = int(input("Número de filas del triángulo: "))
+triangulo_pascal(filas)
