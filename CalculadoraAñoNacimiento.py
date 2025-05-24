c66af6b3b3749ef1e02d621979a490f596b29128
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-23 21:45:18 -0600

    Update CalculadoraAñoNacimiento.py

--- CalculadoraAñoNacimiento.py
+++ CalculadoraAñoNacimiento.py
@@ -3,3 +3,5 @@ from datetime import datetime
 edad = int(input("Ingresa tu edad: "))
 año_actual = datetime.now().year
 año_nacimiento = año_actual - edad
+
+print(f"Probablemente naciste en el año {año_nacimiento}.")
