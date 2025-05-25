2be7005e7c5655e3e37c1c7e1625eadcaac8da8f
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-24 22:27:55 -0600

    Update ConsultorConstantesFisicas.py

--- ConsultorConstantesFisicas.py
+++ ConsultorConstantesFisicas.py
@@ -1,3 +1,4 @@
 constantes = {
     "c": ("Velocidad de la luz", "3.00 x 10^8 m/s"),
     "G": ("Constante gravitacional", "6.674 x 10^-11 N·m²/kg²"),
+    "h": ("Constante de Planck", "6.626 x 10^-34 J·s"),
