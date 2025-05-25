0bf7f0965866da618baff7a52012c8a5118205fe
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-24 22:28:42 -0600

    Update ConsultorConstantesFisicas.py

--- ConsultorConstantesFisicas.py
+++ ConsultorConstantesFisicas.py
@@ -4,3 +4,14 @@ constantes = {
     "h": ("Constante de Planck", "6.626 x 10^-34 J·s"),
     "k": ("Constante de Boltzmann", "1.381 x 10^-23 J/K"),
     "e": ("Carga del electrón", "1.602 x 10^-19 C"),
+    "Na": ("Número de Avogadro", "6.022 x 10^23 mol^-1"),
+    "R": ("Constante de los gases", "8.314 J/mol·K")
+}
+
+clave = input("Ingresa la clave de la constante (c, G, h, k, e, Na, R): ")
+
+if clave in constantes:
+    nombre, valor = constantes[clave]
+    print(f"{nombre}: {valor}")
+else:
+    print("Constante no encontrada.")
