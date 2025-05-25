95829422ef3b6b698c92c02bc2b8c60ae027b40d
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-24 22:28:17 -0600

    Update ConsultorConstantesFisicas.py

--- ConsultorConstantesFisicas.py
+++ ConsultorConstantesFisicas.py
@@ -3,3 +3,4 @@ constantes = {
     "G": ("Constante gravitacional", "6.674 x 10^-11 N·m²/kg²"),
     "h": ("Constante de Planck", "6.626 x 10^-34 J·s"),
     "k": ("Constante de Boltzmann", "1.381 x 10^-23 J/K"),
+    "e": ("Carga del electrón", "1.602 x 10^-19 C"),
