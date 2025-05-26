c888a9d95a98ba06cf362668a05453013aeb6d43
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-25 20:48:35 -0600

    Update ContadorNucleoidosADN.py

--- ContadorNucleoidosADN.py
+++ ContadorNucleoidosADN.py
@@ -2,3 +2,5 @@ def contar_nucleotidos(secuencia):
     secuencia = secuencia.upper()
     conteo = {
         "A": secuencia.count("A"),
+        "T": secuencia.count("T"),
+        "C": secuencia.count("C"),
