a5c2c77ae8f9ec3a1277c2e6876366c2ab87ea31
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-27 20:11:18 -0600

    Update ReconocimientoVozTextoSpeechRecognition.py

--- ReconocimientoVozTextoSpeechRecognition.py
+++ ReconocimientoVozTextoSpeechRecognition.py
@@ -9,3 +9,7 @@ with sr.Microphone() as fuente:
 try:
     texto = reconocedor.recognize_google(audio, language="es-MX")
     print("Texto reconocido:", texto)
+except sr.UnknownValueError:
+    print("No se entendió lo que dijiste.")
+except sr.RequestError:
+    print("Error con el servicio de reconocimiento.")
