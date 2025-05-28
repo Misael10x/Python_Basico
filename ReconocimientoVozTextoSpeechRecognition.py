9dce72c56ff180323eedf642ccef6915bb433e98
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-27 20:11:05 -0600

    Update ReconocimientoVozTextoSpeechRecognition.py

--- ReconocimientoVozTextoSpeechRecognition.py
+++ ReconocimientoVozTextoSpeechRecognition.py
@@ -5,3 +5,7 @@ reconocedor = sr.Recognizer()
 with sr.Microphone() as fuente:
     print("Habla algo...")
     audio = reconocedor.listen(fuente)
+
+try:
+    texto = reconocedor.recognize_google(audio, language="es-MX")
+    print("Texto reconocido:", texto)
