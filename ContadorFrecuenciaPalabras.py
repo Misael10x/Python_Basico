a0ad8cd018b12c884d2431932c88c02ad2ccf818
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-28 20:05:40 -0600

    Update ContadorFrecuenciaPalabras.py

--- ContadorFrecuenciaPalabras.py
+++ ContadorFrecuenciaPalabras.py
@@ -5,4 +5,4 @@ texto = input("Escribe o pega un texto largo:\n")
 palabras = texto.lower().split()
 frecuencia = Counter(palabras)
 
-for palabra, cantidad in
+for palabra, cantidad in frecuencia.most_common():
