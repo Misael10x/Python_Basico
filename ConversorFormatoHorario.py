d9367ae86eef1cfd76c235bc3657960f1cc07196
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-29 21:59:27 -0600

    Update ConversorFormatoHorario.py

--- ConversorFormatoHorario.py
+++ ConversorFormatoHorario.py
@@ -5,3 +5,12 @@ def convertir_hora(hora_24):
     
     if horas == 0:
         return f"12:{minutos} AM"
+    elif horas < 12:
+        return f"{horas}:{minutos} AM"
+    elif horas == 12:
+        return f"12:{minutos} PM"
+    else:
+        return f"{horas - 12}:{minutos} PM"
+
+entrada = input("Ingresa la hora en formato 24h (HH:MM): ")
+print("Formato 12h:", convertir_hora(entrada))
