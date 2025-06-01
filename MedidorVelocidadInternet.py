069de3bca4da500e290edbd948c26da23cb5b83c
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-31 20:25:18 -0600

    Update MedidorVelocidadInternet.py

--- MedidorVelocidadInternet.py
+++ MedidorVelocidadInternet.py
@@ -3,4 +3,6 @@ import speedtest
 st = speedtest.Speedtest()
 descarga = st.download() / 1_000_000
 subida = st.upload() / 1_000_000
-ping = st.results.p
+ping = st.results.ping
+
+print(f"Velocidad de descarga: {descarga:.2f} Mbps")
