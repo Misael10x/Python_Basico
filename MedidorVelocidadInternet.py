d8b9fd53d9dd6e24e54e1d68082332a54f4221bd
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-05-31 20:25:30 -0600

    Update MedidorVelocidadInternet.py

--- MedidorVelocidadInternet.py
+++ MedidorVelocidadInternet.py
@@ -6,3 +6,5 @@ subida = st.upload() / 1_000_000
 ping = st.results.ping
 
 print(f"Velocidad de descarga: {descarga:.2f} Mbps")
+print(f"Velocidad de subida: {subida:.2f} Mbps")
+print(f"Ping: {ping:.2f} ms")
