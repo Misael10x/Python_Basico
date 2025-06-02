6a234b29eaaa0a6e7a9a2417352def4ec9c1784d
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-01 19:38:28 -0600

    Update GeneradorFractalesFormaArbol.py

--- GeneradorFractalesFormaArbol.py
+++ GeneradorFractalesFormaArbol.py
@@ -7,3 +7,17 @@ rama = turtle.Turtle()
 rama.color("lime")
 rama.left(90)
 rama.speed(0)
+
+def arbol(longitud):
+    if longitud < 10:
+        return
+    rama.forward(longitud)
+    rama.left(30)
+    arbol(longitud - 10)
+    rama.right(60)
+    arbol(longitud - 10)
+    rama.left(30)
+    rama.backward(longitud)
+
+arbol(100)
+pantalla.mainloop()
