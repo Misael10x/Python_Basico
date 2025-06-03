22110778ba2e8bbf2dc5e99f9455dbf8a7fa98d4
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-03 17:15:07 -0600

    Update DetectorNumerosPrimosGemelos.py

--- DetectorNumerosPrimosGemelos.py
+++ DetectorNumerosPrimosGemelos.py
@@ -8,3 +8,12 @@ def es_primo(n):
 
 def primos_gemelos(limite):
     gemelos = []
+    for i in range(2, limite-1):
+        if es_primo(i) and es_primo(i+2):
+            gemelos.append((i, i+2))
+    return gemelos
+
+resultado = primos_gemelos(100)
+print("Pares de primos gemelos menores a 100:")
+for par in resultado:
+    print(par)
