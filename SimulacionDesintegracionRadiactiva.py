dbd6dd2a5d74b8adcc81b6ea83089895ba631230
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-02 19:39:02 -0600

    Update SimulacionDesintegracionRadiactiva.py

--- SimulacionDesintegracionRadiactiva.py
+++ SimulacionDesintegracionRadiactiva.py
@@ -8,3 +8,11 @@ n = [n0]
 
 for t in range(1, tiempo):
     desintegrados = np.random.binomial(n[-1], p)
+    n.append(n[-1] - desintegrados)
+
+plt.plot(n, color='orange')
+plt.title("Simulación de Desintegración Radiactiva")
+plt.xlabel("Tiempo")
+plt.ylabel("Átomos restantes")
+plt.grid(True)
+plt.show()
