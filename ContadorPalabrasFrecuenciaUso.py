49eb1b87c0b2ed5ee0dd3da0c2cfa7fee7c75b67
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-04 19:44:22 -0600

    Update ContadorPalabrasFrecuenciaUso.py

--- ContadorPalabrasFrecuenciaUso.py
+++ ContadorPalabrasFrecuenciaUso.py
@@ -1,4 +1,6 @@
 from collections import Counter
 
 texto = """
-La inteligencia artificial está transformando el mundo. Muchas industrias
+La inteligencia artificial está transformando el mundo. Muchas industrias se benefician
+de la automatización, el análisis de datos y la predicción de comportamientos.
+"""
