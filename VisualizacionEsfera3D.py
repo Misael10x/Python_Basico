bb6860ff793dcdf7006f75b2d250092131a8eebb
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-05 18:08:28 -0600

    Update VisualizacionEsfera3D.py

--- VisualizacionEsfera3D.py
+++ VisualizacionEsfera3D.py
@@ -1,5 +1,8 @@
-mport numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
+
+u = np.linspace(0, 2 * np.pi, 100)
+v = np.linspace(0, np.pi, 100)
