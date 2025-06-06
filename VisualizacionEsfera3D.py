f4232d35412c6e42a08112e6417004113a79fcbd
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-05 18:09:23 -0600

    Update VisualizacionEsfera3D.py

--- VisualizacionEsfera3D.py
+++ VisualizacionEsfera3D.py
@@ -6,3 +6,7 @@ ax = fig.add_subplot(111, projection='3d')
 
 u = np.linspace(0, 2 * np.pi, 100)
 v = np.linspace(0, np.pi, 100)
+
+x = 5 * np.outer(np.cos(u), np.sin(v))
+y = 5 * np.outer(np.sin(u), np.sin(v))
+z = 5 * np.outer(np.ones(np.size(u)), np.cos(v))
