033f4844ecb164b9398a5e80dfb065a2530e240b
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-06 20:35:03 -0600

    Update GeneradorTonoSenoidalWAV.py

--- GeneradorTonoSenoidalWAV.py
+++ GeneradorTonoSenoidalWAV.py
@@ -4,3 +4,5 @@ from scipy.io.wavfile import write
 frecuencia = 440       # Frecuencia en Hz (La4)
 duracion = 2           # Duración en segundos
 tasa_muestreo = 44100  # 44.1 kHz
+
+t = np.linspace(0, duracion, int(tasa_muestreo * duracion), False)
