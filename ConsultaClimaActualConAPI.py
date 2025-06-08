d81a6705e3e2492106118dd16036024df5ff672c
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-07 19:58:25 -0600

    Update ConsultaClimaActualConAPI.py

--- ConsultaClimaActualConAPI.py
+++ ConsultaClimaActualConAPI.py
@@ -3,3 +3,5 @@ import requests
 ciudad = "Ciudad de México"
 api_key = "tu_api_key_aqui"  # Regístrate en https://openweathermap.org/ para obtenerla
 url = f"http://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units=metric&lang=es"
+
+respuesta = requests.get(url)
