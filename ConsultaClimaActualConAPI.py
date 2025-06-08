603824c545a2020d982099096baf5c6c2b80d888
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-07 19:58:50 -0600

    Update ConsultaClimaActualConAPI.py

--- ConsultaClimaActualConAPI.py
+++ ConsultaClimaActualConAPI.py
@@ -6,3 +6,10 @@ url = f"http://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key
 
 respuesta = requests.get(url)
 datos = respuesta.json()
+
+if respuesta.status_code == 200:
+    temp = datos["main"]["temp"]
+    descripcion = datos["weather"][0]["description"]
+    print(f"En {ciudad} hay {descripcion} con una temperatura de {temp}°C")
+else:
+    print("No se pudo obtener el clima.")
