53cad5f84d352195bb2b4eb62d98a1d5717654d8
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-11 20:48:10 -0600

    Update AnalizadorSentimientosEspanol.py

--- AnalizadorSentimientosEspanol.py
+++ AnalizadorSentimientosEspanol.py
@@ -5,3 +5,7 @@ def analizar_sentimiento(frase):
     blob = TextBlob(frase)
     sentimiento = blob.sentiment.polarity
     if sentimiento > 0:
+        print("Sentimiento positivo 😊")
+    elif sentimiento < 0:
+        print("Sentimiento negativo 😠")
+    else:
