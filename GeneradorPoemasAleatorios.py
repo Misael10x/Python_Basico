e819fd058d373ce499772fa76eddda2782e81348
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-13 20:21:16 -0600

    Update GeneradorPoemasAleatorios.py

--- GeneradorPoemasAleatorios.py
+++ GeneradorPoemasAleatorios.py
@@ -1,10 +1,9 @@
+import random
+
 sustantivos = ["alma", "luz", "sombra", "tierra", "viento", "mar", "noche"]
-
 verbos = ["llora", "brilla", "cae", "grita", "suspira", "calla", "canta"]
-
 adjetivos = ["oscura", "silente", "eterna", "fugaz", "triste", "brillante", "vacía"]
 
 for _ in range(4):
-
-      linea = f"La {random.choice(sustantivos)} {random.choice(verbos)} en la {random.choice(adjetivos)} noche."
-
+    linea = f"La {random.choice(sustantivos)} {random.choice(verbos)} en la {random.choice(adjetivos)} noche."
+    print(linea)
