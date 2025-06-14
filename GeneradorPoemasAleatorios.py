a5ee6c5b6d31e07a57219056d51fe1223a8a4431
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-13 20:20:09 -0600

    Update GeneradorPoemasAleatorios.py

--- GeneradorPoemasAleatorios.py
+++ GeneradorPoemasAleatorios.py
@@ -3,3 +3,5 @@ sustantivos = ["alma", "luz", "sombra", "tierra", "viento", "mar", "noche"]
 verbos = ["llora", "brilla", "cae", "grita", "suspira", "calla", "canta"]
 
 adjetivos = ["oscura", "silente", "eterna", "fugaz", "triste", "brillante", "vacía"]
+
+for _ in range(4):
