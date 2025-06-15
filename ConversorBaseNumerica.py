12bec2bdeb404fefa1b02a886b200c4fe0a193ca
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-14 20:43:46 -0600

    Update ConversorBaseNumerica.py

--- ConversorBaseNumerica.py
+++ ConversorBaseNumerica.py
@@ -1,8 +1,10 @@
 def convertir_bases(numero):
-
     binario = bin(numero)[2:]
     octal = oct(numero)[2:]
     hexadecimal = hex(numero)[2:].upper()
     return binario, octal, hexadecimal
+
 n = int(input("Ingresa un número decimal: "))
 b, o, h = convertir_bases(n)
+
+print(f"Binario: {b}\nOctal: {o}\nHexadecimal: {h}")
