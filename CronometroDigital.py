a5f85c2a95ad32c9ec33cfb1c71ce5a527e48a87
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-15 20:05:52 -0600

    Update CronometroDigital.py

--- CronometroDigital.py
+++ CronometroDigital.py
@@ -4,3 +4,8 @@ def cronometro():
     input("Presiona ENTER para iniciar el cronómetro...")
     inicio = time.time()
     input("Presiona ENTER para detener el cronómetro.")
+    fin = time.time()
+    transcurrido = fin - inicio
+    print(f"Tiempo transcurrido: {round(transcurrido, 2)} segundos.")
+
+cronometro()
