5d1da39f282dea92d738320cd495843472eae02c
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-17 20:11:36 -0600

    Update DetectorPalindromos.py

--- DetectorPalindromos.py
+++ DetectorPalindromos.py
@@ -1,3 +1,5 @@
 def es_palindromo(texto):
     texto_limpio = ''.join(c.lower() for c in texto if c.isalnum())
     return texto_limpio == texto_limpio[::-1]
+
+entrada = input("Ingresa una palabra o frase: ")
