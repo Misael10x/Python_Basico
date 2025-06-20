6309dec05df0ede1252a65c677a450bbb68c4dc5
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2025-06-19 19:51:33 -0600

    Update GraficoTiempoRealDatosSimulados).py

--- GraficoTiempoRealDatosSimulados).py
+++ GraficoTiempoRealDatosSimulados).py
@@ -6,3 +6,18 @@ valores = []
 
 fig, ax = plt.subplots()
 linea, = ax.plot([], [], lw=2)
+
+def init():
+    ax.set_xlim(0, 50)
+    ax.set_ylim(0, 100)
+    return linea,
+
+def actualizar(frame):
+    if len(valores) >= 50:
+        valores.pop(0)
+    valores.append(random.randint(0, 100))
+    linea.set_data(range(len(valores)), valores)
+    return linea,
+
+ani = animation.FuncAnimation(fig, actualizar, init_func=init, blit=True, interval=200)
+plt.show()
